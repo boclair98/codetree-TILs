8c3d922df20ebd3ced144394364814074f8dec9b
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-11-23 19:08:19 +0900

    Title: 입력받은 값과 합 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231123/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a ,b,n;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        n = a+ b;
+
+        System.out.println(a + " " + b + " " + n);
+    }
+}
