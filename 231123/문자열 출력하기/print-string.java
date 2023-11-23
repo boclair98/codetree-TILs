48d73fef516343b6a050205837f73f4b2ef3f45d
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-11-23 15:01:20 +0900

    Title: 문자열 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231123/문자열 출력하기/print-string.java
@@ -0,0 +1,14 @@
+import Java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n ;
+        n = sc.nextInt();
+        
+        for (int i = 0; i<6; i++ ){
+            System.out.println("LeebrosCode");
+        }
+        
+    }
+}
