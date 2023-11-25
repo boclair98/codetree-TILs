7162f711b795e4697d498a96b9a20816b1d2b1a9
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-11-26 01:58:34 +0900

    Title: 큰 수에서 빼기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 231126/큰 수에서 빼기/subtract-from-large-number.java
+++ 231126/큰 수에서 빼기/subtract-from-large-number.java
@@ -10,7 +10,7 @@ public class Main {
         if (a>=b){
             System.out.println(a-b);
         }
-        if (a<=b){
+        if (a<b){
             System.out.println(b-a);
         }
     }
