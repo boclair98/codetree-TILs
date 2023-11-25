c5ec12f0bd7f0f7631cab716fb11fdf139c55c06
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-11-26 02:11:10 +0900

    Title: 비교에 따른 연산, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 231126/비교에 따른 연산/operation-based-on-comparison.java
+++ 231126/비교에 따른 연산/operation-based-on-comparison.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println(a*b);
         }
         else{
-            System.out.println(b%a);
+            System.out.println(b/a);
         }
         // 여기에 코드를 작성해주세요.
     }
