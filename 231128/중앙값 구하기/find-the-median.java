2822b7144ad9e4ef5a499042e10610551c36732c
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-11-28 17:40:31 +0900

    Title: 중앙값 구하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/중앙값 구하기/find-the-median.java
+++ 231128/중앙값 구하기/find-the-median.java
@@ -8,23 +8,28 @@ public class Main {
         int c = sc.nextInt();
 
         if (a>b){
-            if (c >a){
-                System.out.println(a);
-            }
-            
-        else if (b>c){
-            if (a>b){
-                System.out.println(b);
-            }
-            
-        }
-        else if (a>c){
             if (b>c){
                 System.out.println(b);
             }
-            
-        }
+            else if (c>a){
+                System.out.println(a);
+
+            }
+            else{
+                System.out.println(c);
+            }
         
+        }
+        else{
+            if(a>c){
+                System.out.println(a);
+            }
+            else if (c>b){
+                System.out.println(b);
+            }
+            else{
+                System.out.println(c);
+            }
         }
     }
 }
