4624bba31d8494b36f196224f1b969fad67d0be6
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-11-28 16:43:02 +0900

    Title: 최댓값 구하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/최댓값 구하기/maximum-value.java
+++ 231128/최댓값 구하기/maximum-value.java
@@ -6,27 +6,27 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        if (a>b){
-            if (a>c){
+        if (a>=b){
+            if (a>=c){
                 System.out.println(a);
             }
-            else if (c>a){
+            else if (c>=a){
                 System.out.println(c);
             }
         }
-        else if (b>c){
-            if (b>a){
+        else if (b>=c){
+            if (b>=a){
                 System.out.println(b);
             }
-            else if (a>b){
+            else if (a>=b){
                 System.out.println(a);
             }
         }
-        else if (c>a){
-            if (c>b){
+        else if (c>=a){
+            if (c>=b){
                  System.out.println(c);
             }
-            else if (b>c ){
+            else if (b>=c ){
                  System.out.println(b);
             }
         }
