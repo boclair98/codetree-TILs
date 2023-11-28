59998b231fc6c22c13e143add081cddd2145e87f
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-11-28 15:59:21 +0900

    Title: 장학금, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231128/장학금/scholarship.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        if (n>=90 && m>=95){
+            System.out.println(1000000);
+        }
+        else if (n>=90 && m>=90){
+            System.out.println(50000);
+        }
+        else if (n>=90 && m<90){
+            System.out.println(0);
+        }
+
+        if (n<90){
+            System.out.println(0);
+        }
+    }
+}
