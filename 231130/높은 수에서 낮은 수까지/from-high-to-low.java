f018e9192ac879dbc08ec70dd57b65b53c354501
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-11-30 01:25:27 +0900

    Title: 높은 수에서 낮은 수까지, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 231130/높은 수에서 낮은 수까지/from-high-to-low.java
+++ 231130/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        if (a>b){
+        if (a>=b){
             for (int i=a; i>=b; i--){
                 System.out.print(i+" ");
             }
