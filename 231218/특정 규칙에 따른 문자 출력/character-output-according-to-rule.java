6b937b876147f6367b6b9466b22487d7bd8a71c3
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-12-18 12:41:47 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231218/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=n; i++){
+
+            for(int k = n-i; k>0; k--){
+                System.out.print("  ");
+            }
+
+            for(int t = 1; t<=i; t++){
+                System.out.print("@ ");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i<=n-1; i++){
+
+            for(int x = i; x <= n-1; x++ ){
+                System.out.print("@ ");
+            }
+            System.out.println();
+        }
+
+    }
+}
