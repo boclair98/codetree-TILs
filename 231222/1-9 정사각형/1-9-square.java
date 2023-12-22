7f08f774b0ea47a74c0b114c70f5d0d56e38669c
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-12-22 14:11:21 +0900

    Title: 1-9 정사각형, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231222/1-9 정사각형/1-9-square.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = 1;
+
+        for (int i=0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                if(k%10 == 0){
+                    System.out.print(1);
+                    k++;
+                }
+                else{
+                    System.out.print(k%10);
+                    
+                }
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
