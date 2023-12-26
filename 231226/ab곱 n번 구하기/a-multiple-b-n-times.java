342b63b3e998626b5b782d4e2312f63841e33469
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-12-26 15:00:04 +0900

    Title: ab곱 n번 구하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231226/ab곱 n번 구하기/a-multiple-b-n-times.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int tal = 1;
+            for(int k = a; k<=b; k++){
+                tal = tal * k;
+            }
+            System.out.println(tal);
+        }
+    }
+}
