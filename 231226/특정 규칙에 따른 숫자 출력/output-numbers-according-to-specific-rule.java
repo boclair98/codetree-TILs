06890e88a52d8041cb16be05087757176da3ab5b
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-12-26 12:46:01 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 231226/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
+++ 231226/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
@@ -7,16 +7,18 @@ public class Main {
         int tal = 1;
         for(int i = 0; i<n; i++){
 
-            if(tal >9){
-                tal = 1;
-            }
+            
             for(int k = 0; k<i; k++){
                 System.out.print("  ");
             }
 
             for(int x = 0 ; x<n-i;x++){
+                if(tal >9){
+                tal = 1;
+            }
                 System.out.print(tal+" ");
                 tal++;
+                
             }
             
             System.out.println();
