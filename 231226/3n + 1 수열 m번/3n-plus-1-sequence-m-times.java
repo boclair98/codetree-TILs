8c54e4ee6df5c975e98fb0c6e27bc446f3f58470
boclair98 <108774323+boclair98@users.noreply.github.com>
2023-12-26 15:04:02 +0900

    Title: 3n + 1 수열 m번, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 231226/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
+++ 231226/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -4,9 +4,10 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
-        int cnt = 0;
+        
         for(int i = 1; i<=n; i++){
+            int k = sc.nextInt();
+            int cnt = 0;
             while (true){
                 if (k == 1){
                     break;
