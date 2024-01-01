a36ee68781b3b006a89b4570364d163027b9d959
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-01 20:18:19 +0900

    Title: n개의 숫자 중 최대 2개, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240101/n개의 숫자 중 최대 2개/two-max-of-n-num.java
+++ 240101/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -1,33 +1,21 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[] arr = new int[n];
-
-        int max_val = 0 ;
-        int max2_val = 0 ;
-
-        for (int i = 0 ; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
-
-        for(int i = 0 ; i<n ; i++){
-            if(arr[i]>=max_val){
-                max_val = arr[i];
-            }
-        }
+        Integer[] arr = new Integer[n];
 
         for(int i = 0 ; i<n; i++){
-            if(arr[i]== max_val){
-                continue;
-            }
-            else if(arr[i]>=max2_val){
-                max2_val = arr[i];
-            }
+            arr[i] = sc.nextInt();
         }
-        System.out.printf("%d %d",max_val,max2_val);
+        
+        Arrays.sort(arr,Collections.reverseOrder());
+        
+        System.out.print(arr[0]+" "+arr[1]);
     }
 }
