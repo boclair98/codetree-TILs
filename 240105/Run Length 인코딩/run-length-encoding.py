f46124b2b9005254aaca584646a9922bee2ebf9f
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-05 14:20:38 +0900

    Title: Run Length 인코딩, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/Run Length 인코딩/run-length-encoding.py
+++ 240105/Run Length 인코딩/run-length-encoding.py
@@ -1,18 +1,18 @@
 word = input()
 tal = 1
-val = 0 
-for i in range(1,len(word)):
-    if word[i-1] != word[i] and word[i] !=word[i+1]:
-        pass
-    else:
-        val+=1
-print(val)
+val = 0
+wording = "" 
+
 for i in range(1,len(word)):
    
 
     if word[i-1] == word[i]:
         tal+=1
     else:
-        print(word[i-1]+str(tal),end="")
+        wording+=word[i-1]
+        wording+=str(tal)
         tal = 1
-print(word[-1]+str(tal))
+wording+=word[-1]
+wording+=str(tal)
+print(len(wording))
+print(wording)
