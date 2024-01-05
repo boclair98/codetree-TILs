42efb4ff079702a1af927baaca9c980fffa7521b
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-05 09:48:12 +0900

    Title: 문자열 길이의 합, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240105/문자열 길이의 합/sum-length-of-string.py
+++ 240105/문자열 길이의 합/sum-length-of-string.py
@@ -5,10 +5,6 @@ tal = 0
 for i in range(n):
     word.append(input())
     tal = len(word[i]) + tal
-
-for j in range(n):
-
-    for k in range(len(word[j])):
-        if word[j][k] =='a':
-            val+=1
+    if word[i][0] =='a':
+        val+=1
 print(tal,val)
