3b83ccac0b9abea6846ebbe8e63db0398bdd8fbf
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-07 22:15:37 +0900

    Title: e 제거하기, Time: 228ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240107/e 제거하기/e-to-remove.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+
+        for(int i = 0 ; i<word.length(); i++){
+            if (word.charAt(i) =='e'){
+                word = word.substring(0,i)+word.substring(i+1);
+                break;
+            }
+        }
+        System.out.print(word);
+
+    }
+}
