073d8257fd07646939ddb3a609ebd58812be6b63
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-09 14:41:15 +0900

    Title: 정수만 추출하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240109/정수만 추출하기/extract-only-integers.py
+++ 240109/정수만 추출하기/extract-only-integers.py
@@ -2,10 +2,14 @@ word1, word2 = map(str,input().split())
 tal = ""
 tal2 = ""
 for i in word1:
-    if int(i)>=0 and int(i)<=9:
+    if i>='0' and i<='9':
         tal+=i
+    else:
+        break
 for i in word2:
-    if int(i)>=0 and int(i)<=9:
+    if i>='0' and i<='9':
         tal2+=i
+    else:
+        break
 tal3 = int(tal) + int(tal2)
 print(tal3)
