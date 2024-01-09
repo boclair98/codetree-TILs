e357e0e03e6404a5fabc063e7cb6c6eb973a1872
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-10 02:45:40 +0900

    Title: 최대공약수 구하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240110/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240110/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -3,7 +3,7 @@ a,b = map(int,input().split())
 def mgcd(n,m):
     max_val = 0 
 
-    for i in range(1,n):
+    for i in range(1,m):
         if n%i == 0 and m% i == 0 and max_val < i:
             max_val = i 
     print(max_val)
