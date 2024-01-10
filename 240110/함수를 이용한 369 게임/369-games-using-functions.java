81914d8a3e00feb626ee85a825ccd9f5d81c565a
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-10 11:07:25 +0900

    Title: 함수를 이용한 369 게임, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240110/함수를 이용한 369 게임/369-games-using-functions.java
+++ 240110/함수를 이용한 369 게임/369-games-using-functions.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
 public class Main {
-    public static int num(int z, int x){
-        int cnt = 0;
-        for(int i = z; i<=x; i++){
-            if (i%3==0){
-                cnt++;
+
+    public static boolean is369(int s){
+        while (s>0){
+            if(s%10 == 3 || s%10 == 6 || s%10 == 9){
+                return true;
             }
-            else if (i%10==3 || i%10==6 || i%10 ==9){
-                cnt++;
-                
-            }
-            else if(i/10 ==3 || i/10 ==6 || i/10 ==9){
-                cnt++;
-                
-            }
-            
-            
+            s = s/10;
         }
-        return cnt;
+        return false;
     }
+
+    public static boolean num(int q){
+        return is369(q) ||( q%3 == 0);
+    }
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.print(num(a,b));
+        int cnt = 0 ;
+        for (int i = a; i<=b; i++){
+            if(num(i)){
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
     }
 }
