ff9e446b9a0e022267ad7fb94a8148acb8bbd331
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-10 18:21:03 +0900

    Title: 그 계절, 그 날, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240110/그 계절, 그 날/that-season-that-day.java
+++ 240110/그 계절, 그 날/that-season-that-day.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
-
 public class Main {
 
-    public static boolean yoounun(int k) {
-        if (k >= 1 && k <= 3000) {
-            if (k % 4 != 0) {
+    public static boolean yoounun(int k){
+        if (k>=1 && k<=3000){
+            if (k%4 !=0){
                 return false;
-            } else if (k % 100 != 0) {
+            }
+            else if(k%100 !=0){
                 return true;
-            } else if (k % 400 != 0) {
+
+            }
+            else if(k%400!=0){
                 return false;
-            } else {
+            }
+            else{
                 return true;
             }
-        }
-        return false; // yoounun의 범위를 벗어난 경우
     }
+    return false;
+}
 
-    public static int nowday1(int y, int m) {
+   public static int nowday1(int y, int m) {
         if (m == 2) {
             if (yoounun(y)) {
                 return 29;
@@ -31,28 +34,32 @@ public class Main {
         }
     }
 
-    public static String nowday(int y, int m, int d) {
-        if ((m >= 3 && m <= 5) && (d <= nowday1(y, m))) {
+    public static String nowday(int y, int m, int d){
+        if ((m>=3 &&m<=5) && ( d<= nowday1(y,m))){
             return "Spring";
         }
-        if ((m >= 6 && m <= 8) && (d <= nowday1(y, m))) {
+        if ((m>=6 && m<=8) && ( d<= nowday1(y,m))){
             return "Summer";
         }
-        if ((m >= 9 && m <= 11) && (d <= nowday1(y, m))) {
+        if ((m>=9 &&m<=11) && ( d<= nowday1(y,m))){
             return "Fall";
+            
         }
-        if ((m >= 12 || m <= 2) && (d <= nowday1(y, m))) {
+        if ((m>=12 || m<=2) && ( d<= nowday1(y,m))){
             return "Winter";
         }
-        return "-1"; // 유효하지 않은 경우 -1 반환
+        return "-1";
     }
 
+
+
     public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
         int m = sc.nextInt();
         int d = sc.nextInt();
 
-        System.out.print(nowday(y, m, d));
+        System.out.print(nowday(y,m,d));
     }
 }
