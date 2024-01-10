135e00a87e2d1d323a161d1c8498f2837afc2e64
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-10 18:26:19 +0900

    Title: 두 정수 값 교환하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240110/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void swap(int a , int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.print(a+" "+b);
+
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        swap(n,m);
+    }
+}
