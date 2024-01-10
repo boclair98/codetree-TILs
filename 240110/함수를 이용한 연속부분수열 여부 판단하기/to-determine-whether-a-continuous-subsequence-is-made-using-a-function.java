07a731ab8d056d7da4383caa1137fc5bc7ac10d7
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-10 14:48:47 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 195ms, Memory: 10MB, Status: Passed - CodeTree

--- 240110/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240110/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,46 +1,44 @@
 import java.util.Scanner;
 public class Main {
+    public static int n1,n2;
+    public static int[] a = new int[100];
+    public static int[] b = new int[100];
+    public static boolean issame(int n){
+        for(int i = 0 ; i<n2; i++){
+            if (a[i+n] != b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean didi(){
+        for (int i = 0 ; i<= n1-n2; i++){
+            if (issame(i)){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-
-        int[] arr = new int[101];
-        int[] arr2 = new int[101];
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
 
         for(int i = 0 ; i<n1; i++){
-            arr[i] = sc.nextInt();
+            a[i] = sc.nextInt();
+
         }
 
         for(int i = 0 ; i<n2; i++){
-            arr2[i] = sc.nextInt();
+            b[i] = sc.nextInt();
         }
-        boolean re = false;
-        for(int i = 0 ; i<=n1 - n2 ; i++){
-            int cnt = 0;
-            re = false;
-            for (int j = 0 ; i<n2; j++){
-                if (arr[i+j] == arr2[j]){
-                    cnt++;
-                }
-                else{
-                    break;
-                }
-            }
-            if (cnt == n2){
-                re = true;
-            }
-            if(re){
-                break;
-            }
-        }
-        if (re){
+
+        if (didi()){
             System.out.print("Yes");
         }
         else{
             System.out.print("No");
         }
-        
     }
 }
