e1882056fc224f6986c07a96708dba77945a6229
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-10 10:57:19 +0900

    Title: 함수를 이용한 369 게임, Time: 165ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240110/함수를 이용한 369 게임/369-games-using-functions.java
+++ 240110/함수를 이용한 369 게임/369-games-using-functions.java
@@ -5,13 +5,17 @@ public class Main {
         for(int i = z; i<=x; i++){
             if (i%10==3 || i%10==6 || i%10 ==9){
                 cnt++;
+                
             }
             else if(i/10 ==3 || i/10 ==6 || i/10 ==9){
                 cnt++;
+                
             }
             else if (i%3==0){
                 cnt++;
+                
             }
+            
         }
         return cnt;
     }
