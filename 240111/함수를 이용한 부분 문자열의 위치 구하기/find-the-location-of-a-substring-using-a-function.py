f25c1b1d2aada0e54aeb2e25e94f32a817abf2e6
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-11 11:36:40 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240111/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240111/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -7,7 +7,7 @@ def ind(word,word2):
     for i in range(len(word)-len(word2)+1):
         cnt = 0 
         for j in range(len(word2)):
-            if word[i+j] == word[j]:
+            if word[i+j] == word2[j]:
                 cnt+=1 
         if cnt == len(word2):
             return i
