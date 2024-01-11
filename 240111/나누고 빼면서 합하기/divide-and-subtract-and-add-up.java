226c9cd71d0fb1686c2aaf206f895b782d97dcbb
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-11 14:41:04 +0900

    Title: 나누고 빼면서 합하기, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
+++ 240111/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
@@ -1,32 +1,35 @@
 import java.util.Scanner;
 public class Main {
     public static int n;
-    public static int m ;
+    public static int m;
+    public static int cnt ;
     public static int[] arr = new int[100];
-    public static int cnt;
-    public static int add(){
+
+    public static int add(int[] arr){
         int sum = 0 ;
         while (cnt>=1){
             sum+=arr[cnt];
-            if(cnt %2 == 0){
-                cnt = cnt /2;
+            if (cnt%2 == 0){
+                cnt = cnt / 2;
             }
             else{
-                cnt = cnt - 1;
+                cnt = cnt -1;
             }
         }
         return sum;
-
     }
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+        Scanner sc =new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        cnt = m ;
-        for(int i = 1 ; i<=n; i++ ){
+
+        for (int i = 1 ; i<=n; i++){
             arr[i] = sc.nextInt();
         }
-        System.out.print(add());
+        cnt = m;
+        
+
+        System.out.print(add(arr));
     }
 }
