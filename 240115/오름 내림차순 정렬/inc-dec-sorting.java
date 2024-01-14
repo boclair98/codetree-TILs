a4bf352ae17038fc99a7595b7549762f9231794c
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-15 02:09:57 +0900

    Title: 오름 내림차순 정렬, Time: 296ms, Memory: 15MB, Status: Passed - CodeTree

--- 240115/오름 내림차순 정렬/inc-dec-sorting.java
+++ 240115/오름 내림차순 정렬/inc-dec-sorting.java
@@ -6,7 +6,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[101];
+        int[] arr = new int[n];
 
         for (int i = 0 ; i <n;i++){
             arr[i] = sc.nextInt();
