9fd51c3448a882de7e9d28a51fc5f6b264177b7e
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-15 17:03:38 +0900

    Title: 2개씩 그룹짓기, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/2개씩 그룹짓기/group-of-pairs.py
+++ 240115/2개씩 그룹짓기/group-of-pairs.py
@@ -6,7 +6,8 @@ for i in range(1,n+1):
     tal = tal + arr[-i]
 
 max_val = 0 
-for i in range(n*2):
+num = n*2
+for i in range(0,num - n):
     
     if sum(arr[i:i+n]) < tal and sum(arr[i:i+n])>max_val :
         max_val = sum(arr[i:i+n])
