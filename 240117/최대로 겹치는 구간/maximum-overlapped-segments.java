32c43e5a30e36051c544ff3588fd9c2b578f3b8e
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-17 01:12:02 +0900

    Title: 최대로 겹치는 구간, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240117/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240117/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 public class Main {
+    public static int n;
+    public static int[] num = new int[200];
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] num = new int[202];
+        n = sc.nextInt();
         int max_val = 0 ;
-        int tal = 1;
-        for (int i = 0 ; i<n;i++){
+        for (int i = 0 ; i<n; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
 
-            for(int j = a; j<=b; j++){
-                num[j]+=1;
+            for(int j = a+100; j<b+100; j++){
+                num[j] +=1;
             }
             
-            for(int k = 1; k<202; k++){
-                if (tal <  num[k]){
-                    tal = num[k];
+            for(int k = 0; k<200; k++){
+                if (num[k]>max_val){
+                    max_val = num[k];
                 }
             }
         }
-        System.out.print(tal);
+        System.out.print(max_val);
     }
 }
