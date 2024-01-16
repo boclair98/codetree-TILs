0215d58fd09f5d20d12c39db93b1363a47568021
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-17 03:18:54 +0900

    Title: 왔다 갔던 구역 2, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240117/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240117/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,36 +1,37 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] num = new int[201];
-        int cnt = 0;
-        int cur = 100; // Starting position in the middle
-        int right = 0; // Initialize 'right'
-        int left = 0; 
-
-        for (int i = 0; i < n; i++) {
+        int cnt = 0 ;
+        int cur = 100 ;
+        int right = 0 ;
+        int left = 0 ;
+        for (int i = 0 ; i<n; i++){
             int x = sc.nextInt();
             char r = sc.next().charAt(0);
 
-            if (r == 'R') {
+            if (r == 'R'){
                 right = cur + x;
                 left = cur;
                 cur = right;
-            } else if (r == 'L') {
-                right = cur;
-                left = cur - x;
+                
+            }
+            else if (r=='L'){
+                right = cur ;
+                left = cur-x;
                 cur = left;
+                
             }
-
-            for (int j = left; j < right; j++) {
-                num[j]++;
-            }
+            for(int j = left; j<right; j++){
+                    num[j]++;
+                }
         }
 
-        for (int i = 0; i < 201; i++) {
-            if (num[i] >= 2) {
+        for(int i = 0 ; i<201; i++){
+            if (num[i]>=2){
                 cnt++;
             }
         }
