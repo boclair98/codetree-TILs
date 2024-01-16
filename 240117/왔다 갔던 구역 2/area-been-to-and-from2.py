b535d9ca59f12b3d5caa2350f6a0c27079880f94
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-17 02:39:44 +0900

    Title: 왔다 갔던 구역 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240117/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240117/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -3,7 +3,7 @@ n = int(input())
 num =[0]*201
 
 max_val = 2
-one = 100
+
 cnt = 0 
 for i in range(n):
 
@@ -13,12 +13,12 @@ for i in range(n):
 
     if r == 'R':
 
-        for i in range(101,tal+1):
+        for i in range(100,tal):
             num[i]+=1
     
     elif r == 'L':
         
-        for i in range(tal,tal-x,-1):
+        for i in range(tal-1,tal-x,-1):
             num[i]+=1
     
     
