3b246b34fc1be56600b2a57abcec747706d0825d
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-16 22:05:10 +0900

    Title: 블럭쌓는 명령2, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 240116/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240116/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,14 +2,14 @@ n , k =map(int,input().split())
 
 
 max_val = 0 
+arr = [0] *(n+1)
 for _ in range(k):
-    arr = [0] *(n+1)
+    
     a,b = map(int,input().split())
 
     for i in range(a,b+1):
-        arr[i] = 1
+        arr[i] += 1
     
-    if sum(arr) > max_val:
-        max_val = sum(arr)
+    max_num = max(arr)
 
-print(max_val)
+print(max_num)
