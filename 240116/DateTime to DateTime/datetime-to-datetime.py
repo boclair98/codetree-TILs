6daa5941fb124b005976ab5c64ac827b485cbd25
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-16 13:01:17 +0900

    Title: DateTime to DateTime, Time: 5006ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 240116/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,18 @@
+a,b,c = map(int,input().split())
+
+a1,b1,c1 = 11,11,11
+cnt = 0 
+while True:
+    if a1 == a and b1 ==b and c1 == c:
+        break
+    cnt+=1
+    c1+=1
+    if (c1==60):
+        b1= b1+1
+        c1 = 0
+    
+    if (b1 == 24):
+        a1 = a1+1
+        b1 = 0
+    
+print(cnt)
