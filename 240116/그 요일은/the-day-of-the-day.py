958f318db74e4588302ca33e69e67343ee1128da
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-16 14:52:04 +0900

    Title: 그 요일은, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/그 요일은/the-day-of-the-day.py
+++ 240116/그 요일은/the-day-of-the-day.py
@@ -2,7 +2,7 @@ m1,d1,m2,d2 = map(int,input().split())
 
 word = input()
 month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
-day =['Mon','Tue','Wed','Thu','Fri','Sat','sun']
+day =['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 cnt = 0
 
 def mon(x, y):
