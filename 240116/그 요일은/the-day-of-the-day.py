27dd639d799c86d05a1e7662111bead5aba0728f
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-16 14:38:17 +0900

    Title: 그 요일은, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240116/그 요일은/the-day-of-the-day.py
+++ 240116/그 요일은/the-day-of-the-day.py
@@ -1,23 +1,23 @@
 m1,d1,m2,d2 = map(int,input().split())
 
 word = input()
-month = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
 day =["Mon",'Tue','Wed','Thu','Fri','Sat','Sun']
 cnt = 0
-sat_cnt = 0
-while True:
-    if m1 == m2 and d1 == d2:
+
+def mon(x, y):
+    tal = 0
+
+    for i in range(1,x):
+        tal+=month[i]
+    tal = tal+y
+    return tal
+
+diff = mon(m2,d2) - mon(m1,d1)
+
+for i in range(diff):
+    if day[i%6] == word:
         cnt+=1
-        if day[cnt%7] == word:
-            sat_cnt += 1 
-        break
-    if day[cnt%7] == word:
-        sat_cnt += 1
-    d1 = d1 + 1
-    cnt+=1
-    if d1>month[m1]:
-        m1 = m1 + 1
-        d1 = 1
-    
-    
-print(sat_cnt)
+
+
+print(cnt)
