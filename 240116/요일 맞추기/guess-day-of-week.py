7c8ceeb5c4507545101859156bd529eeb7466614
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-16 13:50:32 +0900

    Title: 요일 맞추기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240116/요일 맞추기/guess-day-of-week.py
+++ 240116/요일 맞추기/guess-day-of-week.py
@@ -30,3 +30,33 @@ elif m1>m2 and d2 > d1:
             if cnt<0:
                 cnt = 6
     print(day[cnt])
+
+elif m1>m2 and d1>d2:
+    while True:
+            if m1 == m2 and d1 == d2:
+                break
+            d1 = d1 - 1
+
+            cnt = cnt - 1
+
+            if d1<1:
+                m1 = m1 - 1
+                d1 = day1[m1]
+            if cnt<0:
+                cnt = 6
+    print(day[cnt])
+
+else:
+    while True:
+            if m1 == m2 and d1 == d2:
+                break
+            d1 = d1 + 1
+
+            cnt = cnt + 1
+
+            if d1>day1[cnt]:
+                m1 = m1 +1
+                d1 =1
+            if cnt>6:
+                cnt = 0
+    print(day[cnt])
