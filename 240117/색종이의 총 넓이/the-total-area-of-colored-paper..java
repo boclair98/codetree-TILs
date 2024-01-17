aea48ec43ff98208e4db9f44dfe119f75761f01e
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-17 23:46:37 +0900

    Title: 색종이의 총 넓이, Time: 160ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240117/색종이의 총 넓이/the-total-area-of-colored-paper..java
+++ 240117/색종이의 총 넓이/the-total-area-of-colored-paper..java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] num = new int[800][800];
+        int[][] num = new int[2000][2000];
         int gijoon = 0 ;
         for(int i = 0 ; i<n; i++){
             int a = sc.nextInt();
