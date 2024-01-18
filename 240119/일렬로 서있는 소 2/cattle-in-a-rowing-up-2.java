91a02ed144628a75544cf4fa57a3dc77c26ae540
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-19 00:04:23 +0900

    Title: 일렬로 서있는 소 2, Time: 174ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240119/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] num = new int[n];
+
+        for(int i = 0 ; i<n; i++){
+            num[i] = sc.nextInt();
+        }
+
+        int cnt = 0 ;
+
+        for(int i = 0 ; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                for(int k = j+1; k<n; k++){
+                    if (num[i]<num[j] && num[j]<num[k]){
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.print(cnt);
+    }
+}
