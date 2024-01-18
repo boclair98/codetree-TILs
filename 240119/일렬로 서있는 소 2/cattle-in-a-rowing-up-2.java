fa056f0259d8655b876240e9bbc8ce4a1a6698b9
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-19 00:06:29 +0900

    Title: 일렬로 서있는 소 2, Time: 275ms, Memory: 10MB, Status: Passed - CodeTree

--- 240119/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 240119/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -15,8 +15,9 @@ public class Main {
         for(int i = 0 ; i<n; i++){
             for(int j = i+1; j<n; j++){
                 for(int k = j+1; k<n; k++){
-                    if (num[i]<num[j] && num[j]<num[k]){
+                    if (num[i]<=num[j] && num[j]<=num[k]){
                         cnt++;
+                        
                     }
                 }
             }
