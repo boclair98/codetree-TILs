4b85cb34a093095b52096e63c9a8193abdb8d54d
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-18 23:46:19 +0900

    Title: 모이자, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/모이자/gather.java
+++ 240118/모이자/gather.java
@@ -16,7 +16,7 @@ public class Main {
         for(int i = 0 ; i<n; i++){
             int sum = 0;
             for(int j = 0 ; j<n; j++){
-                sum =sum+ num[j] + Math.abs(j-i);
+                sum =sum+ num[j] * Math.abs(j-i);
             }
             ans = Math.min(sum,ans);
         }
