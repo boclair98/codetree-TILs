4cafbf0f72515af424a59f9b5b6fcd8eb6a02a97
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-18 23:45:45 +0900

    Title: 모이자, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240118/모이자/gather.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] num = new int[n];
+
+        for(int i = 0 ; i<n; i++){
+            num[i] = sc.nextInt();
+        }
+        int ans = INT_MAX;
+
+        for(int i = 0 ; i<n; i++){
+            int sum = 0;
+            for(int j = 0 ; j<n; j++){
+                sum =sum+ num[j] + Math.abs(j-i);
+            }
+            ans = Math.min(sum,ans);
+        }
+        System.out.print(ans);
+
+
+    }
+}
