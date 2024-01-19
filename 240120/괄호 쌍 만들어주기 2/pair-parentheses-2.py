f4f1d4f1a69436517389774807eb887d26c80dae
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-20 03:50:14 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240120/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,10 @@
+word = list(input())
+n = len(word)
+cnt = 0 
+for i in range(n):
+
+    for j in range(i+1,n-1):
+
+        if word[i]=="(" and word[i+1]=="(" and word[j]==')'and word[j+1] ==")":
+            cnt+=1
+print(cnt)
