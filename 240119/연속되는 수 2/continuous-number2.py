8187e89b2ba1ead14ed49452cb0f42e4e32db7e7
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-19 12:22:53 +0900

    Title: 연속되는 수 2, Time: 153ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240119/연속되는 수 2/continuous-number2.py
+++ 240119/연속되는 수 2/continuous-number2.py
@@ -4,9 +4,14 @@ for _ in range(n):
     num.append(int(input()))
 
 cnt = 0 
-
+tal = 0 
 for i in range(n-1):
 
     if num[i] == num[i+1]:
         cnt+=1
-print(cnt+1)
+    
+    else:
+        cnt = 1
+    
+    tal = max(tal,cnt)
+print(tal)
