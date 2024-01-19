68b3398d3fc95050a37aed6cffa5ae5b5e84bef7
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-19 12:10:13 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240119/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240119/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,37 +1,40 @@
 n = int(input())
 
-color  = [False] * 2001
+# color 리스트를 False 대신 None으로 초기화
+color = [None] * 2001
 
 num = [0] * 2001
 right = 0 
 left = 0 
 cur  = 1000
+
 for i in range(n):
-    a , b = map(str,input().split())
+    a, b = map(str, input().split())
     a = int(a)
 
     if b == 'R':
-
         right = cur + a
         left = cur
         cur = right
-        for j in range(left,right):
-            num[j]+=1
+        for j in range(left, right):
+            num[j] += 1
             color[j] = "Black"
+
     if b == 'L':
-        right = right 
+        right = right
         left = right - a
         cur = left
-        for j in range(left,right):
-            num[j]+=1
+        for j in range(left, right):
+            num[j] += 1
             color[j] = "White"
-    
-wh = 0 
+
+wh = 0
 bl = 0
 
 for i in range(2001):
-    if num[i]>0 and color[i] == "White":
-        wh+=1
-    if num[i]>0 and color[i] == "Black":
-        bl+=1
-print(wh,bl)
+    if num[i] > 0 and color[i] == "White":
+        wh += 1
+    if num[i] > 0 and color[i] == "Black":
+        bl += 1
+
+print(wh, bl)
