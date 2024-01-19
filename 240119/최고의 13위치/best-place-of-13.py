b8830a0a7ebbf74f6dc1ab75d1dfb95add61cb48
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-19 11:50:49 +0900

    Title: 최고의 13위치, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+num = []
+
+for _ in range(n):
+    num.append(list(map(int,input().split())))
+
+cnt = 0 
+
+for i in range(n):
+
+    for j in range(n - 2):
+
+        cnt = max(cnt, num[i][j] + num[i][j+1]+num[i][j+2])
+
+print(cnt)
