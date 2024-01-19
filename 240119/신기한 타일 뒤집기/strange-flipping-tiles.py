bd310b5feec9e93dae4beb63adfad02e3e40bb50
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-19 12:11:25 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240119/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240119/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,12 +1,12 @@
 n = int(input())
 
-# color 리스트를 None으로 초기화
+# color 리스트를 False 대신 None으로 초기화
 color = [None] * 2001
 
 num = [0] * 2001
-right = 0
-left = 0
-cur = 1000
+right = 0 
+left = 0 
+cur  = 1000
 
 for i in range(n):
     a, b = map(str, input().split())
@@ -16,19 +16,17 @@ for i in range(n):
         right = cur + a
         left = cur
         cur = right
-        for j in range(left, right):
-            if j >= 0 and j < 2001:  # 인덱스가 범위 내에 있는지 확인
-                num[j] += 1
-                color[j] = "Black"
+        for j in range(left+1, right+1):
+            num[j] += 1
+            color[j] = "Black"
 
     if b == 'L':
         right = right
         left = right - a
         cur = left
-        for j in range(left, right):
-            if j >= 0 and j < 2001:  # 인덱스가 범위 내에 있는지 확인
-                num[j] += 1
-                color[j] = "White"
+        for j in range(left+1, right+1):
+            num[j] += 1
+            color[j] = "White"
 
 wh = 0
 bl = 0
