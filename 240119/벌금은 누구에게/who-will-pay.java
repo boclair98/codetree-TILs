124754602ccc2b715356dba2b32799d3a8b1e414
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-19 15:39:44 +0900

    Title: 벌금은 누구에게, Time: 275ms, Memory: 19MB, Status: Passed - CodeTree

--- 240119/벌금은 누구에게/who-will-pay.java
+++ 240119/벌금은 누구에게/who-will-pay.java
@@ -6,22 +6,20 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int k = sc.nextInt();
-        int[] num = new int[101];
+        int[] num = new int[n+1];
         int cnt1 = 1;
-        for(int i = 1; i<=n; i++){
-            num[i] = cnt1;
-            cnt1++;
-        }
+        
         int ans = -1;
+        
         for(int i = 0 ; i<m; i++){
-            int now = sc.nextInt();
-            num[now]+=1;
-            if(num[now]==k){
-                ans = now ;
-                break; 
-            }
+            int count = sc.nextInt();
+            num[count] = num[count]+1;
+            if(num[count]>=k){
+                ans = count;
+                break;
+            }    
         }
         System.out.print(ans);
-
+        
     }
 }
