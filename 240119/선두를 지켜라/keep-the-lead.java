29221e2830fff9b9614dea041ca098f392135753
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-19 16:15:05 +0900

    Title: 선두를 지켜라, Time: 306ms, Memory: 28MB, Status: Passed - CodeTree

--- 240119/선두를 지켜라/keep-the-lead.java
+++ 240119/선두를 지켜라/keep-the-lead.java
@@ -38,7 +38,7 @@ public class Main {
         }
         int leader = 0;
         int cntall = 0 ;
-        for(int i = 1 ;i<1000; i++){
+        for(int i = 0 ;i<1000001; i++){
             if (asok[i]>bsok[i]){
                 if(leader==2){
                     cntall++;
