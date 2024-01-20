f8920df8ac9e77cfcfc599012eb7ed6d0acfa64c
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-20 21:05:56 +0900

    Title: 아름다운 수열 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240120/아름다운 수열 2/beautiful-sequence-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] num1 = new int[n];
+        int[] num2 = new int[m];
+
+        for(int i = 0; i<n; i++){
+            num1[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i<m; i++){
+            num2[i] = sc.nextInt();
+        }
+        int tot = 0;
+        for(int i =0; i<=n-m;i++){
+            int cnt = 0;
+            for(int j = 0; j<m; j++){
+                if (num1[i]==num2[j] || num1[i+1]==num2[j] || num1[i+2]==num2[j]){
+                    cnt++;
+                }
+            }
+            if (cnt == m){
+                tot++;
+            }
+            
+        }
+        System.out.println(tot);
+        
+    }
+}
