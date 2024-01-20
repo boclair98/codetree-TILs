5b3d76add2f9ca133870371a7a8014d0cfbba818
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-20 14:31:39 +0900

    Title: 오목, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240120/오목/O-mok.py
+++ 240120/오목/O-mok.py
@@ -24,7 +24,7 @@ for i in range(19):
                 omok_true = True
                 cntx = i
                 cnty = j+2
-                tal = 2
+                tal = 1
                 break
         if omok[j][i] == 1 and omok[j+1][i]==1 and omok[j+2][i]==1 and omok[j+3][i]==1 and omok[j+4][i] == 1:
                 omok_true = True
@@ -35,3 +35,22 @@ for i in range(19):
 if omok_true==True:
     print(tal)
     print(cntx+1,cnty+1)
+elif omok_true ==False:
+
+    for i in range(15):
+        for j in range(15):
+            if omok[i][j] == 1 and omok[i+1][j+1] == 1 and omok[i+2][j+2]==1 and omok[i+3][j+3] ==1 and omok[i+4][j+4] == 1:
+                omok_true=True
+                cntx = i
+                cnty = j+2
+                tal = 1
+                break
+            if omok[i][j] == 2 and omok[i+1][j+1] == 2 and omok[i+2][j+2]==2 and omok[i+3][j+3] ==2 and omok[i+4][j+4] == 2:
+                omok_true=True
+                cntx = i
+                cnty = j+2
+                tal = 2
+                break
+    if omok_true==True:
+        print(tal)
+        print(cntx+1,cnty+1)
