4671352cc3381bd8def2166abd3e18485ad34dea
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-20 20:01:18 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240120/G or H 3/G-or-H-3.java
+++ 240120/G or H 3/G-or-H-3.java
@@ -16,7 +16,7 @@ public class Main {
             
         }
         int ans = -1000000;
-        for(int i = 0; i<1000001; i++){
+        for(int i = 0; i<100001; i++){
             int tal = 0;
             for(int j = i; j<=i+m; j++){
                 if(word[j]=='G'){
