8e587baa9c76d0ddad0441a88c6d430340b8737f
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-23 00:30:56 +0900

    Title: 1이 3개 이상 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240123/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ 240123/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 public class Main {
-        public static int[] xd= new int[]{1,0,-1,0};
-        public static int[] yd = new int[]{0,-1,0,1};
-        public static int n;
-        public static int[][] arr = new int[100][100];
-
-        public static boolean intrange(int x ,int y){
-
-            return 0<=x && x<n && 0<=y && y<n;
-        }
+    public static int n;
+    public static int[][] arr = new int[100][100];
+    public static int[] x_s = new int[]{1,0,-1,0};
+    public static int[] y_s = new int[]{0,-1,0,1};
+    public static boolean inrange(int x, int y){
+        return x>=0 && x<n && y>=0 && y<n;
+    }
     public static int dfs(int x, int y){
         int tal = 0 ;
-
-        for(int i = 0 ; i<4; i++){
-            int dx=x+xd[i], dy=y+yd[i];
-            if(intrange(dx,dy) && arr[dx][dy]==1){
+        for(int i = 0 ; i<n; i++){
+            int nx = x+x_s[i] , ny = y+y_s[i];
+            if (inrange(nx,ny)&&arr[nx][ny]==1){
                 tal++;
             }
         }
@@ -24,28 +21,22 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        
-
         for(int i = 0 ; i<n; i++){
             for(int j = 0 ; j<n; j++){
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        xd = new int[]{1,0,-1,0};
-        yd= new int[]{0,-1,0,1};
-
-        int cnt = 0;
+        int cnt = 0 ;
         for(int i = 0 ; i<n; i++){
-
             for(int j = 0 ; j<n; j++){
-
                 if(dfs(i,j)>=3){
                     cnt++;
                 }
             }
         }
         System.out.print(cnt);
+
 
     }
 }
