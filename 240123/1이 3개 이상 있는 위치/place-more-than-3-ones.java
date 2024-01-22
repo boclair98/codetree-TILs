8423e6890a52b70df3ce8d1cb9c2db8352e43b7c
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-23 00:31:49 +0900

    Title: 1이 3개 이상 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240123/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ 240123/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 public class Main {
     public static int n;
-    public static int[][] arr = new int[100][100];
+    public static int[][] arr = new int[101][101];
     public static int[] x_s = new int[]{1,0,-1,0};
     public static int[] y_s = new int[]{0,-1,0,1};
+
     public static boolean inrange(int x, int y){
         return x>=0 && x<n && y>=0 && y<n;
     }
+
     public static int dfs(int x, int y){
         int tal = 0 ;
         for(int i = 0 ; i<n; i++){
             int nx = x+x_s[i] , ny = y+y_s[i];
-            if (inrange(nx,ny)&&arr[nx][ny]==1){
+            if (inrange(nx,ny) && arr[nx][ny]==1){
                 tal++;
             }
         }
         return tal;
     }
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
