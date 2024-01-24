463f5fc1511acf6e2c1208b0285e1a62cfaaf515
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-24 23:14:27 +0900

    Title: 특정 수와 근접한 합, Time: 160ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240124/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240124/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -8,11 +8,9 @@ for i in combinations(num,s-2):
     tal = sum(i)
     list1.append(tal)
 
-for i in range(len(list1)):
-    list1[i] = list1[i] - n
 list1.sort()
 
-for j in range(len(list1)):
-    if list1[j] >0:
-        print(list1[j])
+for i in range(len(list1)):
+    if list1[i]>= n :
+        print(list1[i]-n)
         break
