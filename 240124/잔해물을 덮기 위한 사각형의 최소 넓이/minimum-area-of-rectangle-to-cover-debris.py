0dff5e2b7e9dc9ad5d357ea74294ae54f74e4d73
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-24 16:08:48 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 192ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240124/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240124/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -2,15 +2,7 @@ a,b,c,d = map(int,input().split())
 a1,b1,c1,d1 = map(int,input().split())
 
 square = [[0]*2001 for _ in range(2001)]
-a+=500
-b+=500
-c+=500
-d+=500
 
-a1+=500
-b1+=500
-c1+=500
-d1+=500
 for i in range(a,c):
     for j in range(b,d):
         square[i][j] = 1
