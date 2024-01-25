3efc6998006b7d25b9ea3885a29d418817069dfa
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-26 03:15:20 +0900

    Title: 운행 되고 있는 시간, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240126/운행 되고 있는 시간/hours-in-service.py
+++ 240126/운행 되고 있는 시간/hours-in-service.py
@@ -3,6 +3,8 @@ n = int(input())
 time = [tuple(map(int,input().split())) for _ in range(n)]
 time.sort()
 ans = 0 
+tal = 0
+cnt = 0
 for i in range(n):
     x_time = time[i][0]
     y_time = time[i][1]
@@ -15,5 +17,6 @@ for i in range(n):
         if y_time < time[j][0]:
 
             cnt = y_time - x_time + time[j][1] - time[j][0]
-    ans = max(ans , cnt)  
+        tal = max(tal,cnt)
+    ans = max(ans , tal)  
 print(ans)
