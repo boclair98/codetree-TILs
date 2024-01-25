4d04c84c94d06fc17033dc9f8180f3a0a52b5605
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-25 16:12:54 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240125/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240125/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -4,19 +4,13 @@ INTMAX= sys.maxsize
 num = [tuple(map(int,input().split())) for _ in range(n)]
 ans = INTMAX
 for i in range(n):
-    min_x,max_x =INTMAX,0
-    min_y,max_y = INTMAX,0
+    tal1 = num[i][0]
+    tal2 = num[i][1]
+    
 
-    for j,(x,y) in enumerate(num):
-
-        if i == j :
+    for j ,(x,y) in enumerate(num):
+        if i == j:
             continue
-
-        min_x = min(x,min_x)
-        max_x = max(x,max_x)
-        min_y = min(y,min_y)
-        max_y = max(y,max_y)
-
-    ans = min(ans,((max_x - min_x) *(max_x - min_x) + (max_y -min_y)*(max_y-min_y)) )
-
+        
+        ans = min(ans,(tal1 - x)**2 + (tal2 -x) **2)
 print(ans)
