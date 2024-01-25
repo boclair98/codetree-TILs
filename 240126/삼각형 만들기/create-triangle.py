582255100595b83703625f452264cdcd5aaf5aca
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-26 02:58:52 +0900

    Title: 삼각형 만들기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240126/삼각형 만들기/create-triangle.py
@@ -0,0 +1,25 @@
+import sys
+
+INTMAX = sys.maxsize
+n = int(input())
+tri = [tuple(map(int,input().split())) for _ in range(n)]
+ans = -INTMAX
+
+for i in range(n):
+
+    max_x,min_x=0,INTMAX
+    max_y,min_y=0,INTMAX
+
+    for j,(x,y) in enumerate(tri):
+
+        if i == j :
+            continue
+        
+        max_x = max(x, max_x)
+        min_x = min(x, min_x)
+        max_y = max(y, max_y)
+        min_y = min(y, min_y)
+    
+    ans = max(ans, (max_x - min_x) * (max_y - min_y))
+    
+print(ans)
