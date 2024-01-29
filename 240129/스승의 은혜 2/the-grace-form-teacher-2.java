4db6f4c2a00314f3cfbade81c6b5bded86e02b19
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-29 14:29:38 +0900

    Title: 스승의 은혜 2, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240129/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 240129/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -1,40 +1,42 @@
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int p = sc.nextInt();
-        int[] student = new int [1001];
-
-        for(int i = 1 ; i <= n ; i++){
-            student[i] = sc.nextInt();
+        
+        // 학생 수와 예산 입력
+        int N = sc.nextInt();
+        int B = sc.nextInt();
+        
+        // 각 학생의 선물 가격 입력 및 정렬
+        int[] gifts = new int[N];
+        for (int i = 0; i < N; i++) {
+            gifts[i] = sc.nextInt();
         }
-        Arrays.sort(student);
-        int coin = 0 ;
-        int index = 0;
-        int count = 0 ;
-        while (true){
-            
-            coin = coin + student[index];
-            
-            if (coin > p ){
-                coin = coin - (student[index]) + (student[index] / 2);
-                if (coin <= p){
-                    
+        Arrays.sort(gifts);
+
+        // 선물 가능한 학생의 최대 명수 계산
+        int maxStudents = 0;
+        int remainingBudget = B;
+
+        for (int i = 0; i < N; i++) {
+            if (remainingBudget >= gifts[i]) {
+                remainingBudget -= gifts[i];
+                maxStudents++;
+            } else {
+                // 쿠폰 사용
+                remainingBudget += (gifts[i] / 2);
+                
+                // 쿠폰 사용 후 여전히 예산이 모자라면 더 이상 확인하지 않고 종료
+                if (remainingBudget < gifts[i]) {
                     break;
                 }
                 
+                maxStudents++;
             }
-            
-            if (index == n-1){
-                break;
-            }
-            index++;
-            count++;
-            
         }
-        System.out.print(count);
+
+        System.out.println(maxStudents);
     }
 }
