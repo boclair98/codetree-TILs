e4be11451023f04fb72a9dd0f2caf8f956a88a55
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-29 13:57:27 +0900

    Title: 스승의 은혜 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240129/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p = sc.nextInt();
+        int[] student = new int [n];
+
+        for(int i = 0 ; i < n ; i++){
+            student[i] = sc.nextInt();
+        }
+        Arrays.sort(student);
+        int coin = 0 ;
+        int index = 0;
+        while (true){
+            if (coin > p ){
+                coin = coin - (student[index] / 2);
+                if (coin < p){
+                    break;
+                }
+            }
+            coin = coin + student[index];
+            index++;
+        }
+        System.out.print(index);
+    }
+}
