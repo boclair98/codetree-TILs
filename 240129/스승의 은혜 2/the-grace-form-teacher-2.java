ab71be916ec5d55a27b2ccb5186fa3d15bc73e4b
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-01-29 14:02:34 +0900

    Title: 스승의 은혜 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240129/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 240129/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -25,7 +25,7 @@ public class Main {
                 }
                 
             }
-            if (index > student.length){
+            if (index == n){
                 break;
             }
             index++;
