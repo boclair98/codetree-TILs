1f434faabba4cbeb513735254c6342053abe0a11
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-02-07 15:30:21 +0900

    Title: 1이 3개 이상 있는 위치, Time: 128ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240207/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10**6)
+n = int(input())
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+dx = [1,0,-1,0]
+dy = [0,-1,0,1]
+cnt = 0
+def dfs(x,y):
+    global cnt
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<n:
+            if graph[nx][ny] == 1:
+                cnt+=1
+    return cnt
+total = 0
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            if dfs(i,j)>=3 :
+                total+=1
+print(total)
