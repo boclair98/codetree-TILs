cfda6354c62fa7d7c702ffb26c99847a9b916c62
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-02-07 14:11:30 +0900

    Title: 흰검 칠하기, Time: 211ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240207/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] tile = new int[2001];
+        String[] visited = new String[2001];
+        
+        for(int i = 0; i<2001; i++){
+            visited[i]="No";
+        }
+        int left = 1000;
+        int right = 1000;
+
+        for (int i = 0; i<n; i++){
+            int a = sc.nextInt();
+            char word = sc.next().charAt(0);
+
+            if (word=='R'){
+                right = left + a;
+                
+                for (int j = left; j<right; j++){
+                    visited[j] ="Black";
+                    tile[j] +=1;
+                }
+                
+            }
+
+            if(word=='L'){
+                left = right  - a;
+                for (int j = left; j<right; j++){
+                    visited[j] ="White";
+                    tile[j] +=1;
+                }
+            }
+            
+        }
+        int white = 0;
+        int black = 0;
+        int gray = 0 ;
+        for (int i = 0; i<2001; i++){
+            if(visited[i].equals("White")&& tile[i]<4){
+                white+=1;
+            }
+            if (visited[i].equals("Black") && tile[i]<4){
+                black+=1;
+            }
+            if(tile[i]>3){
+                gray+=1;
+            }
+        }
+        System.out.print(white+" "+black+" "+gray);
+    }
+}
