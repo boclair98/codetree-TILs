41f58900dfb8d4882a31cac1abc06e6da8223e55
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-02-07 15:01:20 +0900

    Title: 문자에 따른 명령 2, Time: 166ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240207/문자에 따른 명령 2/text-based-commands2.java
+++ 240207/문자에 따른 명령 2/text-based-commands2.java
@@ -4,8 +4,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
-        int[] dy = new int[]{1,-1,0,0};
-        int[] dx = new int[]{0,0,-1,1};
+        int[] dx = new int[]{1,0,-1,0};
+        int[] dy = new int[]{0,-1,0,1};
         int x = 0;
         int y = 0;
         int start = 3;
