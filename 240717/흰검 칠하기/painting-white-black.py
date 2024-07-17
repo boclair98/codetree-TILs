9095987dddca3ab7315e56e6a572d1db25206ef3
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-07-17 18:46:41 +0900

    Title: 흰검 칠하기, Time: 137ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240717/흰검 칠하기/painting-white-black.py
+++ 240717/흰검 칠하기/painting-white-black.py
@@ -1,7 +1,7 @@
 n = int(input())
 
-visited = [0] *(1000000)
-color = ['Yellow'] *(1000000)
+visited = [0] *(10000)
+color = ['Yellow'] *(10000)
 now = 100 
 for _ in range(n):
     s,r = input().split() 
@@ -11,18 +11,21 @@ for _ in range(n):
         for i in range(now,now-s,-1):
             visited[i]+=1
             color[i] = 'White'
-        now = now - s +1
+        now = now - s+1 
     elif r == 'R':
         for i in range(now,now+s):
             visited[i]+=1
             color[i] = 'Black'
         now=now+s-1
-
-for i in range(1000000):
-    if visited[i] >3:
-        color[i] ='gray'
-
-white = color.count('White')
-gray = color.count('gray')
-Black = color.count("Black")
-print(white,Black,gray)
+gray = 0
+white = 0
+black = 0
+for i in range(10000):
+    if visited[i] >=4:
+        gray+=1
+    elif visited[i]>=2:
+        if color[i] =='Black':
+            black+=1
+        else:
+            white+=1
+print(white,black,gray)
