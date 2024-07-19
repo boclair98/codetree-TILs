6fa6f2aeabcce5a9cdf8b72483b305faadf5babc
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-07-19 16:48:39 +0900

    Title: 행복한 수열의 개수, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240719/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240719/행복한 수열의 개수/number-of-happy-sequence.py
@@ -6,24 +6,26 @@ for i in range(n):
     cnt = 1
     first = num[i][0] 
     for j in range(1,n):
+        if cnt >= m:
+            break 
         if first != num[i][j]:
             first = num[i][j]
-            
+            cnt = 1 
         else:
-            
             cnt+=1
     if cnt >=m:
         total+=1
-   
+
 for i in range(n):
     cnt1 = 1 
     two = num[0][i]
     for j in range(1,n):
+        if cnt1 >= m:
+            break
         if two != num[j][i]:
             two = num[j][i]
-
+            cnt1 = 1
         else:
-            
             cnt1+=1
     if cnt1>=m:
         total+=1
