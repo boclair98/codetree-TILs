0cc9f283c7a307e6a6b8a8a0b2ccf24ec814119c
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-07-19 16:35:15 +0900

    Title: 행복한 수열의 개수, Time: 113ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240719/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240719/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,28 +1,26 @@
 n, m = map(int,input().split())
 num = [list(map(int,input().split())) for _ in range(n)]
 total = 0
-if m  == 1:
-    print(n+n)
-else:
-    for i in range(n):
-        cnt = 1
-        first = num[i][0] 
-        for j in range(1,n):
-            if first != num[i][j]:
-                first = num[i][j]
-            else:
-                cnt+=1
-        if cnt >=m:
-            total+=1
+
+for i in range(n):
+    cnt = 1
+    first = num[i][0] 
+    for j in range(1,n):
+        if first != num[i][j]:
+            first = num[i][j]
+        else:
+            cnt+=1
+    if cnt >=m:
+        total+=1
    
-    for i in range(n):
-        cnt1 = 1 
-        two = num[0][i]
-        for j in range(1,n):
-            if two != num[j][i]:
-                two = num[j][i]
-            else:
-                cnt1+=1
-        if cnt1>=m:
-            total+=1
-    print(total)
+for i in range(n):
+    cnt1 = 1 
+    two = num[0][i]
+    for j in range(1,n):
+        if two != num[j][i]:
+            two = num[j][i]
+        else:
+            cnt1+=1
+    if cnt1>=m:
+        total+=1
+print(total)
