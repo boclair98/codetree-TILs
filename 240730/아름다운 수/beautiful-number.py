eccad9e5879eb5511799c1b4e16d3b674fc88348
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-07-30 12:56:22 +0900

    Title: 아름다운 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240730/아름다운 수/beautiful-number.py
+++ 240730/아름다운 수/beautiful-number.py
@@ -6,67 +6,26 @@ total = 0
     
 def check(number):
     idx = 0
-    su = 0 
-    fail = 0
-    if number.count('1') == n:
-        return True   
-    elif number.count('2') == n:
-        if n % 2 == 0:
-            return True 
-        else:
-            return False
     while idx < n:
-        if number[idx] =='1':
-            su+=1
-            idx+=1
-
-        elif number[idx]=='2' :
-            if idx == n-1:
-                fail+=1
-                break
-            if number[(idx+1)%n]=='2':
-                su+=1
-                idx+=2
-            else:
-                fail+=1
-                idx+=1 
-
-        elif number[idx]=='3' :
-            if idx == n-2 or idx==n-1:
-                fail+=1
-                break
-            if number[(idx+1)%n]=='3' and number[(idx+2)%n]=='3':
-                su+=1
-                idx+=3
-            else:
-                fail+=1
-                idx+=1
-
-        elif number[idx]=='4':
-            if idx == n-3:
-                fail+=1
-                break
-            if number[(idx+1)%n] =='4' and number[(idx+2)%n]=='4' and number[(idx+3)%n]=='4':
-                su+=1
-                idx+=4
-            else:
-                idx+=1
-                fail+=1
-    if su>0 and fail == 0:
-        return True
-    return False
+        if idx + int(number[idx]) -1 >=n:
+            return False 
+        for j in range(idx,idx+int(number[idx])):
+            if number[idx] != number[j]:
+                return False 
+        idx+=int(number[idx])
+    return True
+            
 
 def backtracking():
     global total
     if len(num) == n :
-        number = "".join(map(str,num))
+        number = list("".join(map(str,num)))
         checks = check(number)
         if checks:
-            
             total+=1
         return 
 
-    for i in range(1,n+1):
+    for i in range(1,5):
         visited[i] = True 
         num.append(i)
         backtracking()
