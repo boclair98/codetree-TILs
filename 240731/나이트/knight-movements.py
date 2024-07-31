d4edfefd68544033b94317f16098bc14e7c529d3
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-07-31 23:38:47 +0900

    Title: 나이트, Time: 186ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240731/나이트/knight-movements.py
+++ 240731/나이트/knight-movements.py
@@ -1,27 +1,34 @@
 from collections import deque 
 n = int(input())
 sx,sy,ex,ey = map(int,input().split())
+sx,sy,ex,ey = sx-1,sy-1,ex-1,ey-1
 num = [[0]*n for _ in range(n)]
 dx = [2,2,1,-1,-2,-2,-1,1]
 dy = [-1,1,2,2,1,-1,-1,-1]
 visited = [[False]*n for _ in range(n)]
-def bfs(x,y,ex,ey):
+cnt = 0 
+def bfs(x,y,ex,ey,count):
     q = deque()
     q.append((x,y))
     while q: 
-        x1,y1 = q.popleft() 
+        x1,y1= q.popleft()
+        count = 1
         for i in range(8):
             nx = x1 + dx[i]
             ny = y1 + dy[i]
-            if nx == ex and ny == ey : 
-                return num[nx-1][ny-1]
+            if nx == ex and ny == ey :
+                num[ex][ey] = count  
+                return num[ex][ey]
             if 0<=nx<n and 0<=ny<n:
                 if visited[nx][ny] == False:
                     visited[nx][ny] = True 
                     q.append((nx,ny))
-                    num[nx][ny] = num[x1][y1]+1 
+                    num[nx][ny]  = count 
+                    count+=1
+                    
+                    
     
     return -1
 
 
-print(bfs(sx-1,sy-1,ex-1,ey-1)+1)
+print(bfs(sx,sy,ex,ey,1)+1)
