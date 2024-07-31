9315b2d6128a72c68b9d6cf207d2355eff01883a
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-07-31 23:17:03 +0900

    Title: 나이트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240731/나이트/knight-movements.py
@@ -0,0 +1,27 @@
+from collections import deque 
+n = int(input())
+sx,sy,ex,ey = map(int,input().split())
+num = [[0]*n for _ in range(n)]
+dx = [2,2,1,-1,-2,-2,-1,1]
+dy = [-1,1,2,2,1,-1,-1,-1]
+visited = [[False]*n for _ in range(n)]
+def bfs(x,y,ex,ey):
+    q = deque()
+    q.append((x,y))
+    while q: 
+        x1,y1 = q.popleft() 
+        for i in range(8):
+            nx = x1 + dx[i]
+            ny = y1 + dy[i]
+            if nx == ex and ny == ey : 
+                return num[nx-1][ny-1]
+            if 0<=nx<n and 0<=ny<n:
+                if visited[nx][ny] == False:
+                    visited[nx][ny] = True 
+                    q.append((nx,ny))
+                    num[nx][ny] = num[x1][y1]+1 
+    
+    return num 
+
+
+print(bfs(sx,sy,ex,ey)+1)
