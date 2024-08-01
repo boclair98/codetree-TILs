de5f981651640d082a5bc2dff316804ec0e06a3d
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-01 15:59:48 +0900

    Title: 겹치지 않는 선분 2, Time: 138ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240801/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240801/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -14,5 +14,7 @@ for i in range(n):
         if x1<sx<y1 and x1<sy<y1:
             cnt.add((sx,sy))
             cnt.add((x1,y1))
-
-print(n-len(cnt))
+        if y1<sx<x1 and y1<sy<x1:
+            cnt.add((sx,sy))
+            cnt.add((x1,y1))
+print(n - len(cnt))
