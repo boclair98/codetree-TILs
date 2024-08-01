d91e6f2926b775f6da21e63cce7ce103146cb7b7
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-01 16:13:50 +0900

    Title: 겹치지 않는 선분 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240801/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240801/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -3,18 +3,18 @@ num = []
 for _ in range(n):
     x,y = map(int,input().split())
     num.append((x,y))
-cnt = set()
-cnt2 = [] 
+cnt = 0
+
 for i in range(n):
     sx,sy = num[i]
+    flag =True 
     for j in range(n):
         if i == j :
             continue 
         x1,y1 = num[j]
-        if x1<=sx<=y1 and x1<sy<=y1:
-            cnt.add((sx,sy))
-            cnt.add((x1,y1))
-        elif y1<=sx<=x1 and y1<=sy<=x1:
-            cnt.add((sx,sy))
-            cnt.add((x1,y1))
-print(len(num)-  len(cnt))
+        if (sx <=x1 and sy>=y1) or (sx>=x1 and sy<=y1):
+            flag = False 
+            break 
+    if flag == False:
+        cnt+=1
+print(cnt)
