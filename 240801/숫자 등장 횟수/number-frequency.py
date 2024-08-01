e3e66457261c82281a1393becc739e158879c31b
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-01 16:42:49 +0900

    Title: 숫자 등장 횟수, Time: 180ms, Memory: 34MB, Status: Passed - CodeTree

--- 240801/숫자 등장 횟수/number-frequency.py
+++ 240801/숫자 등장 횟수/number-frequency.py
@@ -1,7 +1,7 @@
 n, m = map(int,input().split())
 num = list(map(int,input().split()))
 dic = {}
-x,y = map(int,input().split())
+mm = list(map(int,input().split()))
 
 for i in range(n):
     if num[i] not in dic:
@@ -9,11 +9,8 @@ for i in range(n):
     else:
         dic[num[i]]+=1
 
-if x in dic:
-    print(dic[x],end=' ')
-else:
-    print(0,end=' ')
-if y in dic:
-    print(dic[y],end=' ')
-else:
-    print(0,end=' ')
+for i in range(m):
+    if mm[i] in dic:
+        print(dic[mm[i]],end=' ')
+    else:
+        print(0,end=' ')
