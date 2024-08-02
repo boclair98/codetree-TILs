af151e79be5e9509e3ac157d88a7b146682cb106
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-02 16:38:22 +0900

    Title: 돌 잘 치우기, Time: 481ms, Memory: 28MB, Status: Passed - CodeTree

--- 240802/돌 잘 치우기/clear-stones-well.py
+++ 240802/돌 잘 치우기/clear-stones-well.py
@@ -1,63 +1,57 @@
-from collections import deque
+from collections import deque 
+from copy import deepcopy
 n,k,m = map(int,input().split())
-#격자크기 n, 시작점의 수 k, 치워야할 돌의 개수 m 
-
 num = [list(map(int,input().split())) for _ in range(n)]
+
+count = 0 
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
-
-number = []
-
-def first_bfs(i,j):
-    cnt = 0 
-    q = deque()
-    q.append((i,j))
-    while q : 
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<n:
-                if num[nx][ny] == 0:
-                    cnt+=1
-                elif num[nx][ny] ==1 :
-                    cnt-=1
-    return cnt
-
-def bfs(i,j):
-    q = deque()
-    q.append((i,j))
-    visited[i][j] = True
-    cnt = 1  
-    while q :
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<n:
-                if visited[nx][ny] == False and num[nx][ny] == 0:
-                    cnt+=1
-                    visited[nx][ny] = True 
-                    q.append((nx,ny))
-    return cnt  
+one = []
+start= [] 
+for i in range(k):
+    a,b = map(int,input().split())
+    a-=1
+    b-=1
+    start.append((a,b))
 
 
 
 for i in range(n):
     for j in range(n):
-        if num[i][j] == 1 : 
-            number.append((i,j,first_bfs(i,j)))
-number.sort(key = lambda x : (-x[2],-x[0]))
+        if num[i][j] == 1:
+            one.append((i,j))
 
-for i in range(m):
-    x,y,cnt = number[i]
-    num[x][y] = 0
+def bfs(num):
+    visited = [[False]*n for _ in range(n)] 
+    cnt = 1
+    q = deque(start)
+    visited[q[0][0]][q[0][1]] = True 
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<n :
+                if visited[nx][ny] == False and num[nx][ny] == 0:
+                    visited[nx][ny] = True 
+                    q.append((nx,ny))
+                    cnt+=1
+    return cnt 
 
-max_val = 0 
-for _  in range(k):
-    x , y = map(int,input().split())
-    x = x - 1
-    y = y - 1
-    visited = [[False]*n for _ in range(n)]
-    max_val = max(max_val,bfs(x,y))
-print(max_val)
+result = 0 
+def dfs(count,idx):
+    global result
+    if count == m:
+        graph = deepcopy(num)
+        result = max(result,bfs(graph))
+        return 
+    
+    if idx>= len(one):
+        return 
+    i,j = one[idx]
+    num[i][j] = 0 
+    dfs(count+1,idx+1)
+    num[i][j] = 1
+    dfs(count,idx+1)
+dfs(0,0)
+print(result)
