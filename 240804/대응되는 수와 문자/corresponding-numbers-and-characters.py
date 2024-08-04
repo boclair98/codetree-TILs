2e7b3cf82796ec021f9cad78c35b3216057aa0be
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-04 18:38:00 +0900

    Title: 대응되는 수와 문자, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 240804/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -1,11 +1,15 @@
 n, m = map(int,input().split())
 dic = {i:input() for i in range(1,n+1)} 
 
-
 for _ in range(m):
     s = input()
     if s.isdigit():
         print(dic[int(s)])
     else:
-        dic2= {y:x for x,y in dic.items()}
-        print(dic2[s])
+        idx = 1
+        while idx < n:
+            if dic[idx] == s:
+                print(idx)
+                break
+            else:
+                idx+=1
