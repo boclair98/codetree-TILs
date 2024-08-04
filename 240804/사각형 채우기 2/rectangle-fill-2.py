9bb00aa4b8327bf8814b3475dc88cdbf77578469
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-04 15:20:41 +0900

    Title: 사각형 채우기 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/사각형 채우기 2/rectangle-fill-2.py
+++ 240804/사각형 채우기 2/rectangle-fill-2.py
@@ -4,7 +4,14 @@ dp = [0] *(n+1)
  
 dp[1] = 1 
 dp[2] = 3
-
+# 3 = 5 
+# 4 = 11 
+# 5 = 21
+# 6 = 43 
+# 7 = 85 
 for i in range(3,n+1):
-    dp[i] = dp[i-1] * dp[i-2]
+    if i % 2 == 1:
+        dp[i] = dp[i-1] * 2 - 1
+    else:
+        dp[i] = dp[i-1] * 2 + 1
 print(dp[n])
