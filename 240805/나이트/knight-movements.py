3a7afa63be2b9239819ba9012b48050b7a35913e
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-05 13:52:24 +0900

    Title: 나이트, Time: 1514ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240805/나이트/knight-movements.py
+++ 240805/나이트/knight-movements.py
@@ -2,6 +2,7 @@ from collections import deque
 
 n = int(input())
 r1,c1,r2,c2 = map(int,input().split())
+r1,c1,r2,c2 = r1-1,c1-1,r2-1,c2-1
 chess = [[0]*n for _ in range(n)]
 visited = [[False]*n for _ in range(n)]
 dx = [-2,-2,-1,1,2,2,1,-1]
