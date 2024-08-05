c5ac1b49842e2beff88c1632a4b07a1324bac4fb
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-05 14:44:33 +0900

    Title: 비를 피하기, Time: 173ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240805/비를 피하기/stay-out-of-rain.py
+++ 240805/비를 피하기/stay-out-of-rain.py
@@ -1,8 +1,19 @@
-from collections import deque 
+from collections import deque
+import copy
 n,h,m = map(int,input().split())
 rain = [list(map(int,input().split())) for _ in range(n)]
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
+rain2 = copy.deepcopy(rain)
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+fir_visited = [[False]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if rain[i][j] == 1 or rain[i][j] == 3:
+            if fir_visited[i][j] == False:
+                fir_visited[i][j] = True 
+
 def bfs(i,j):
     cnt = 0 
     q = deque()
@@ -14,24 +25,20 @@ def bfs(i,j):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0<=nx<n and 0<=ny<n:
-                if visited[nx][ny] == False and rain[nx][ny]==0:
-                    cnt+=1
-                    visited[nx][ny] = True
-                    q.append((nx,ny))  
-                elif visited[nx][ny] == False and rain[nx][ny] == 3:
+                if visited[nx][ny] == False and rain[nx][ny] == 3 :
                     cnt+=1 
                     return cnt 
+                elif visited[nx][ny] == False and (rain[nx][ny] == 0 or rain[nx][ny] == 2):
+                    visited[nx][ny] = True 
+                    cnt+=1 
+                    q.append((nx,ny))
     return -1 
+
 for i in range(n):
     for j in range(n):
         if rain[i][j] == 2:
             visited = [[False]*n for _ in range(n)]
             tal=bfs(i,j)
-            rain[i][j] = tal
-for i in range(n):
-    for j in range(n):
-        if rain[i][j] == 1 or rain[i][j]==3:
-            print(0,end=' ')
-        else:
-            print(rain[i][j],end=' ')
-    print()
+            rain2[i][j] = tal
+print(rain)
+print(rain2)
