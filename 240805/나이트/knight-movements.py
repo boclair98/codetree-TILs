a0cd73b97787b7b327af3e97137199dca26fba51
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-05 13:59:36 +0900

    Title: 나이트, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240805/나이트/knight-movements.py
+++ 240805/나이트/knight-movements.py
@@ -6,10 +6,11 @@ r1,c1,r2,c2 = r1-1,c1-1,r2-1,c2-1
 chess = [[0]*n for _ in range(n)]
 dx = [-2,-2,-1,1,2,2,1,-1]
 dy = [-1,1,2,2,1,-1,-2,-2]
-
+visited= [[False]*n for _ in range(n)]
 def bfs(i,j):
     q = deque()
     q.append((i,j))
+    visited[i][j] = True 
     if i == r2 and j == c2:
         return 0 
     while q: 
@@ -20,8 +21,9 @@ def bfs(i,j):
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<n:
+            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
                 q.append((nx,ny))
+                visited[nx][ny] = True 
                 chess[nx][ny] = chess[x][y] + 1 
                 if nx == r2 and ny == c2:
                     return chess[r2][c2]
