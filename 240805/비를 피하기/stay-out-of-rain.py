cfdee748411d3b3aa4d873572c24f6861fa329d8
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-05 15:05:42 +0900

    Title: 비를 피하기, Time: 233ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240805/비를 피하기/stay-out-of-rain.py
+++ 240805/비를 피하기/stay-out-of-rain.py
@@ -3,42 +3,48 @@ import copy
 n,h,m = map(int,input().split())
 rain = [list(map(int,input().split())) for _ in range(n)]
 rain2 = copy.deepcopy(rain)
-
+rain3 = [[0]* n for _ in range(n)]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 fir_visited = [[False]*n for _ in range(n)]
 for i in range(n):
     for j in range(n):
-        if rain[i][j] == 1 or rain[i][j] == 3:
+        if rain[i][j] == 1 or rain[i][j] == 3 or rain[i][j]==0:
             if fir_visited[i][j] == False:
-                fir_visited[i][j] = True 
+                fir_visited[i][j] = True
 
 def bfs(i,j):
-    cnt = 0 
     q = deque()
     q.append((i,j))
-    visited[i][j] = True 
-    while q:
+    while q: 
         x,y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0<=nx<n and 0<=ny<n:
-                if visited[nx][ny] == False and rain[nx][ny] == 3 :
-                    cnt+=1 
-                    return cnt 
-                elif visited[nx][ny] == False and (rain[nx][ny] == 0 or rain[nx][ny] == 2):
+                if visited[nx][ny] == False and ((rain[nx][ny]==0 or rain[nx][ny]==2)):
                     visited[nx][ny] = True 
-                    cnt+=1 
                     q.append((nx,ny))
-    return -1 
+                    rain3[nx][ny] = rain3[x][y] + 1 
+                elif visited[nx][ny] == False and (rain[nx][ny]==3):
+                    rain3[nx][ny] = rain3[x][y] + 1 
+                    return rain3[nx][ny] 
+    return -1
+                
 
 for i in range(n):
     for j in range(n):
-        if rain[i][j] == 2:
-            visited = [[False]*n for _ in range(n)]
-            tal=bfs(i,j)
-            rain2[i][j] = tal
-print(rain)
-print(rain2)
+        if rain2[i][j] == 2 : 
+            visited = [[False] * n for _ in range(n)] 
+            visited[i][j] = True
+            tal = bfs(i,j)
+            rain2[i][j] = tal 
+
+for i in range(n):
+    for j in range(n):
+        if fir_visited[i][j] == True :
+            print(0,end=' ')
+        else:
+            print(rain2[i][j],end=' ')
+    print()
