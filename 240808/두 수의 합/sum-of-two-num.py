b2f0a4613bfd0bfdadd4f96845870e79d5f922fe
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-08 10:32:57 +0900

    Title: 두 수의 합, Time: 1080ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240808/두 수의 합/sum-of-two-num.py
+++ 240808/두 수의 합/sum-of-two-num.py
@@ -1,12 +1,17 @@
 n, k = map(int,input().split())
-num = [list(map(int,input().split()))]
+num = list(map(int,input().split()))
 
 p_t = {}
 cnt = 1
 for i in range(n):
-    for j in range(n):
-        if i == j:
-            continue 
-        if p_t not in cnt:
-            pt[cnt] = num[i] + num[j]
-print(pt)
+    for j in range(i+1,n):
+        if cnt not in p_t:
+            p_t[cnt] = num[i] + num[j]
+            cnt+=1
+idx = 1 
+count = 0
+while idx < cnt :
+    if p_t[idx] == k:
+        count+=1
+    idx+=1
+print(count)
