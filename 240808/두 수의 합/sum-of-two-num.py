d8910e00f42274c8cdf56a6a5e38b1b95f9087ab
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-08 10:33:45 +0900

    Title: 두 수의 합, Time: 1268ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240808/두 수의 합/sum-of-two-num.py
+++ 240808/두 수의 합/sum-of-two-num.py
@@ -5,6 +5,8 @@ p_t = {}
 cnt = 1
 for i in range(n):
     for j in range(i+1,n):
+        if i == j:
+            continue
         if cnt not in p_t:
             p_t[cnt] = num[i] + num[j]
             cnt+=1
