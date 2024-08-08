53d7bf1a3ec38097f6d7605e388a83f2519a4120
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-08 11:17:00 +0900

    Title: 삼각형 컨베이어 벨트, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240808/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,16 @@
+from collections import deque 
+n,t = map(int,input().split())
+num1 = list(map(int,input().split()))
+num2 = list(map(int,input().split()))
+num3 = list(map(int,input().split()))
+tot = num1 + num2 + num3
+tot = deque(tot)
+count = 0 
+while count < t :
+    tot.appendleft(tot.pop())
+    count+=1
+
+while len(tot) > 0:
+    for i in range(n):
+        print(tot.popleft(),end=' ')
+    print()
