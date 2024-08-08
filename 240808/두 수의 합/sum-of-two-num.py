3b4a06166e8b86870cbf32c00ab10e4fd82e72e1
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-08 10:36:17 +0900

    Title: 두 수의 합, Time: 5098ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 240808/두 수의 합/sum-of-two-num.py
+++ 240808/두 수의 합/sum-of-two-num.py
@@ -5,14 +5,6 @@ p_t = {}
 cnt = 1
 for i in range(n):
     for j in range(i+1,n):
-        if i == j:
-            continue
-        p_t[cnt] = num[i] + num[j]
-        cnt+=1
-idx = 1 
-count = 0
-while idx < cnt :
-    if p_t[idx] == k:
-        count+=1
-    idx+=1
-print(count)
+        if num[i] + num[j] == k:
+            cnt+=1
+print(cnt-1)
