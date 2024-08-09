185dbe2fce2bddff8c0424c4c7e434cc623173fc
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-09 21:13:57 +0900

    Title: 선분 위의 점, Time: 535ms, Memory: 33MB, Status: Passed - CodeTree

--- 240809/선분 위의 점/point-on-the-line-segment.py
+++ 240809/선분 위의 점/point-on-the-line-segment.py
@@ -1,13 +1,32 @@
 n, m = map(int,input().split())
 num = list(map(int,input().split()))
-val = [0] * 1000000
-for i in num:
-    val[i]+=1
+num.sort()
+def binary(s):
+    start = 0
+    end = n - 1
+    idx = n 
+    while start <=end:
+        mid = (start+end) // 2
+        if num[mid]>s:
+            idx = min(idx,mid)
+            end = mid - 1
+        else:
+            start = mid + 1
+    return idx
+
+def binary2(s):
+    start = 0
+    end = n - 1
+    idx = n 
+    while start <=end:
+        mid = (start+end) // 2
+        if num[mid]>=s:
+            idx = min(idx,mid)
+            end = mid - 1
+        else:
+            start = mid + 1
+    return idx 
 
 for _ in range(m):
     s,y = map(int,input().split())
-    cnt = 0 
-    for i in range(s,y+1):
-        if val[i] == 1:
-            cnt+=1
-    print(cnt)
+    print(binary(y) - binary2(s))
