ce3021a831707e5a9a60a46c4cee51d7d107147f
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-09 21:04:15 +0900

    Title: 선분 위의 점, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240809/선분 위의 점/point-on-the-line-segment.py
@@ -0,0 +1,13 @@
+n, m = map(int,input().split())
+num = list(map(int,input().split()))
+val = [0] * 100000
+for i in num:
+    val[i]+=1
+
+for _ in range(m):
+    s,y = map(int,input().split())
+    cnt = 0 
+    for i in range(s,y+1):
+        if val[i] == 1:
+            cnt+=1
+    print(cnt)
