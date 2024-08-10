0588f1a1b0c54d9d0aeb50865ef825e2f3e9ad58
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-10 16:24:16 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 140ms, Memory: 28MB, Status: Passed - CodeTree

--- 240810/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
+++ 240810/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
@@ -1,18 +1,17 @@
 m = int(input())
 a,b = map(int,input().split())
 num = []
-for i in range(1,m+1):
-    num.append(i)
+
 def binary(x):
-    start = 0 
-    end = m - 1
+    start = 1
+    end = m 
     count = 0 
     while start<=end : 
         mid = (start + end ) // 2 
-        if num[mid] == x:
+        if mid == x:
             count+=1
             return count 
-        elif num[mid] > x:
+        elif mid > x:
             end = mid - 1
             count+=1
         else:
