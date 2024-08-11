486257e40f3b700aa103364fed06b617244b4746
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-11 16:48:53 +0900

    Title: 자연수 n개의 합, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/자연수 n개의 합/sum-of-n-natural-numbers.py
+++ 240811/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -1,12 +1,12 @@
 n = int(input())
-max_v = 10000000
+max_v = 2*10**9
 max_val = 0
 start = 1 
 end = max_v
 while start<=end : 
     mid = (start+end) // 2 
-    if mid * (mid+1)//2 <=s:
-        left =mid + 1
+    if mid * (mid+1) //2 <=n:
+        start =mid + 1
         max_val = max(max_val,mid)
     else:
         end = mid - 1
