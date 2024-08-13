3009bc6c2b5c7a9affe7e18318a379a23401eee0
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-13 11:51:17 +0900

    Title: xor 결과 최대 만들기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240813/xor 결과 최대 만들기/max-of-xor.py
+++ 240813/xor 결과 최대 만들기/max-of-xor.py
@@ -1,1 +1,28 @@
-print(1^3^5)
+n, m = map(int,input().split())
+num = list(map(int,input().split()))
+
+tal = [] 
+max_val = 0 
+visited = [False] * n
+def xor(tal):
+    num = 0 
+    for i in range(1,m):
+        num=(tal[i-1] ^ tal[i])
+    return num 
+
+def back(s):
+    global max_val
+    if len(tal) == m:
+        max_val = max(max_val,xor(tal))
+
+        return
+    
+    for i in range(s,n):
+        if visited[i] == False:
+            visited[i] = True
+            tal.append(num[i])
+            back(i+1)
+            visited[i] = False
+            tal.pop()
+back(0)
+print(max_val)
