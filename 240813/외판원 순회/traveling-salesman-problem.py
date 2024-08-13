0ecaac1a75a10206ba2cc8e4d09cf5a5693ddaa9
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-13 13:19:36 +0900

    Title: 외판원 순회, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240813/외판원 순회/traveling-salesman-problem.py
+++ 240813/외판원 순회/traveling-salesman-problem.py
@@ -4,16 +4,17 @@ visited = [False] * n
 hap = 10000000  
 def back(s,total):
     global hap
-    if s>=n:
+    if s==n:
         hap = min(hap,total)
-        return
-
+        return 
+    
     for i in range(n):
+        if num[s][i] == 0:
+            continue
         if visited[i] == False:
-            visited[i] = True 
-            back(s+1,total+num[i][s])
+            visited[i] = True
+            back(s+1,total+num[s][i]) 
             visited[i] = False
-
-
+    
 back(0,0)
 print(hap)
