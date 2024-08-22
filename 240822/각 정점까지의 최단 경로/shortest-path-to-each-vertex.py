e1e3bcbe80bbd8069cc12de01c02672d19e70690
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-22 15:51:17 +0900

    Title: 각 정점까지의 최단 경로, Time: 871ms, Memory: 49MB, Status: Passed - CodeTree

--- 240822/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
+++ 240822/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -10,6 +10,7 @@ graph = [[] for _ in range(n+1)]
 for _ in range(m):
     u,v,w = map(int,input().split())
     graph[u].append((v,w))
+    graph[v].append((u,w))
 
 def dstra(start):
     q = [] 
@@ -30,6 +31,6 @@ def dstra(start):
 dstra(start)
 for i in range(1,n+1):
     if distance[i] == INF:
-        print(1)
+        print(-1)
     else:
         print(distance[i])
