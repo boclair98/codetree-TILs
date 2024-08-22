65fcc803f1db085ef6ccb44b9eab10775523a67d
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-08-22 15:22:55 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240822/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -0,0 +1,35 @@
+import sys 
+import heapq
+INF = sys.maxsize
+n,m = map(int,input().split())
+
+dist = [INF] *(n+1)
+
+edge = [[] for _ in range(n+1)]
+
+visited = [False] * (n+1)
+
+for _ in range(m):
+    u,v,w = map(int,input().split())
+    edge[u].append((v,w))
+
+
+def daijkstra(start):
+    q = [] 
+    heapq.heappush(q,(0,start))
+    dist[start] = 0 
+
+    while q: 
+        distance, node = heapq.heappop(q)
+        if dist[node] < distance:
+            continue 
+        
+        for nexts in edge[node]:
+            cost = dist[node] + nexts[1]
+            if cost < dist[nexts[0]]:
+                dist[nexts[0]] = cost 
+                heapq.heappush(q,(cost,nexts[0]))
+
+daijkstra(1)
+for i in range(2,n+1):
+    print(dist[i])
