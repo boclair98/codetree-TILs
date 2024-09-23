3795b6681cb3478de4bb6dfb29622d9057966c41
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-24 01:26:39 +0900

    Title: 변수 값 교체하기, Time: 80ms, Memory: 8MB, Status: Passed - CodeTree

--- 240924/변수 값 교체하기/replacing-variable-values.java
+++ 240924/변수 값 교체하기/replacing-variable-values.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a = 3;
-        int a = 6;
+        a = 6;
         System.out.println(a);
     }
 }
