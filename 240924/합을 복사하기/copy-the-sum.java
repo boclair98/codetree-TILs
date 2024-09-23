d8174df4eb1e5c3b37ec2e2ac4a698d20eb5fbf0
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-24 01:37:25 +0900

    Title: 합을 복사하기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240924/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int total = a + b + c;
+        a = total;
+        b = total;
+        c = total;
+        System.out.println(a+" "+b+" "+c);
+    }
+}
