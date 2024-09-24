dc7d5fdacc422a6f019705e2ee98599e5ff292b7
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-25 01:24:10 +0900

    Title: 알파벳에 따른 평가, Time: 115ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240925/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        if (s.equals('S')){
+            System.out.print("Superior");
+        }else if(s.equals("A")){
+            System.out.print("Excellent");
+        }else if(s.equals("B")){
+            System.out.print("Good");
+        }else if(s.equals("C")){
+            System.out.print("Usually");
+        }else if(s.equals("D")){
+            System.out.print("Effort");
+        }
+    }
+}
