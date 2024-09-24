4fcd484c390c5805083bf80228e74d2e31353d48
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-24 11:05:00 +0900

    Title: 입력받아 계산 4, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240924/입력받아 계산 4/input-calculate-4.java
+++ 240924/입력받아 계산 4/input-calculate-4.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a  = sc.nextInt();
-        a = a * 2
+        a = a * 2;
         System.out.println(a);
     }
 }
