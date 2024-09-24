44b7be0e288bb450f3ddfb51e3bd9e3e103524ba
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-24 11:24:06 +0900

    Title: 문자열 받아 출력, Time: 172ms, Memory: 10MB, Status: Passed - CodeTree

--- 240924/문자열 받아 출력/enter-word-and-print.java
+++ 240924/문자열 받아 출력/enter-word-and-print.java
@@ -4,6 +4,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        System.out.prinln(s);
+        System.out.println(s);
     }
 }
