7b7388012a947649e0bd774f7f0826084f07cabe
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-25 14:35:34 +0900

    Title: 점수 비교, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240925/점수 비교/score-comparison.java
+++ 240925/점수 비교/score-comparison.java
@@ -5,9 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
         int c = sc.nextInt();
-        int d = sc.nextInt():
+        int d = sc.nextInt();
 
         if(a>c && b>d){
             System.out.println(1);
