38a8a98a129ab2619d50bc01ebf57928b9a5fcd6
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-25 15:03:11 +0900

    Title: 두 사람, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240925/두 사람/two-person.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int ag1 = sc.nextInt();
+        String s1 = sc.next();
+
+        int ag2 = sc.nextInt();
+        String s2 = sc.next();
+
+        if((ag1>=19 || ag2>=19) &&(s1.equals("M") || s2.equals("M")) ){
+            System.out.println(1);
+        }else{
+            System.out.println(2);
+        }
+}
+}
