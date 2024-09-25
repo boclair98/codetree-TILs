9ce48b37db53ff88de50d6c0e25070d71736f342
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-25 15:22:54 +0900

    Title: 일 수 구하기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240925/일 수 구하기/number-of-days-in-month.java
+++ 240925/일 수 구하기/number-of-days-in-month.java
@@ -7,7 +7,7 @@ public class Main {
         if(n%2==0){
             if(n==2){
                 System.out.println(28);
-            }else if(n==8 || n==10){
+            }else if(n==8 || n==10 || n == 12){
                 System.out.println(31);
             }else{
                 System.out.println(30);
