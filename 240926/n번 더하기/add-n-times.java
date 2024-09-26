9ef587b641a8fdcc433e1d54d71d58e0eb2e1f72
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-26 18:06:24 +0900

    Title: n번 더하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240926/n번 더하기/add-n-times.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a , n ;
+        a = sc.nextInt();
+        n = sc.nextInt();
+        int start = 1;
+        while (start<=n){
+            a+=n;
+        }
+        System.out.print(a);
+}
