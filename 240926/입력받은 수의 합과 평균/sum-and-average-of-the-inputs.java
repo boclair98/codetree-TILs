71e0569278408641a0d70e9050f259196747a785
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-26 19:28:57 +0900

    Title: 입력받은 수의 합과 평균, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240926/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int total = 0;
+        int cnt = 0 ;
+        for(int i = 0 ; i<n; i++){
+            int s = sc.nextInt();
+            total=s
+        }
+        double avg = (double) total/n;
+        System.out.printf("%d %.1f",total,avg);
+    }
+}
