665d77d5dce1f1fb356b0e925a562ec83f7fb8de
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-26 11:38:18 +0900

    Title: 증가시키며 출력하기 3, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 240926/증가시키며 출력하기 3/increase-and-print-3.java
+++ 240926/증가시키며 출력하기 3/increase-and-print-3.java
@@ -3,7 +3,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int i = 10;
         while(i<=26){
-            System.out.println(i+" ");
+            System.out.print(i+" ");
             i+=2;
         }
     }
