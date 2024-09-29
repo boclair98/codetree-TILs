7f4673b5b25c464c68b1842419f2007b7833c81a
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-29 18:48:35 +0900

    Title: 별표 출력하기 11, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/별표 출력하기 11/print-star-11.java
+++ 240929/별표 출력하기 11/print-star-11.java
@@ -5,12 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i =0; i<2*n+1; i++){
-            if(i%2 == 0){
-                System.out.println("* * * * * * * ");
-            }else{
-                System.out.println("*   *   *   *");
+        for(int i =1; i<=2*n+1; i++){
+            for(int j = 1 ; j<=2*n+1; j++){
+                if(i%2 ==0 && j%2 == 0){
+                    System.out.print("  ");
+                }else{
+                    System.out.print("* ");
+                }
             }
+            System.out.println();
         }
     }
 }
