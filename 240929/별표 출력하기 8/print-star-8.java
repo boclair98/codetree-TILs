a8670c212a6d2a0de896f969dff7bc8cf508d2e6
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-09-29 13:21:31 +0900

    Title: 별표 출력하기 8, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/별표 출력하기 8/print-star-8.java
+++ 240929/별표 출력하기 8/print-star-8.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        int cnt = 1;
         for(int i = 1; i<=n; i++){
             if(i%2 == 1){
                 System.out.println("*");
             }else{
-                for(int j = 1; j<=2*i; j++){
+                for(int j = 1; j<=2*(cnt); j++){
                     System.out.print("* ");
                 }
+                cnt++;
                 System.out.println();
             }
         }
