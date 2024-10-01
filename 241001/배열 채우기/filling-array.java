6062143e73babb09987c467bb1f5833ba1d1ce1a
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-10-01 16:00:12 +0900

    Title: 배열 채우기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241001/배열 채우기/filling-array.java
+++ 241001/배열 채우기/filling-array.java
@@ -4,19 +4,17 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
-
-        int cnt = 0;
-        while (true){
+        int cnt = 0 ;
+        for(int i = 0 ; i<10; i++){
             int n = sc.nextInt();
             if(n==0){
                 break;
-            }else{
-            arr[cnt] = n ;
-            cnt++;}
+            }
+            arr[i] = n ;
+            cnt++;
         }
-        for(int i = cnt-1; i>=0; i--){
-            System.out.print(arr[i]+" ");
+        for(int j = cnt-1; j>=0; j--){
+            System.out.print(arr[j]+" ");
         }
-
     }
 }
