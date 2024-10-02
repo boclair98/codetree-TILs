db624da2c9ab12cdc0e840d9cc8ef0c85720ca1b
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-10-02 17:22:34 +0900

    Title: 나눗셈의 나머지, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241002/나눗셈의 나머지/remainder-of-division.java
+++ 241002/나눗셈의 나머지/remainder-of-division.java
@@ -15,7 +15,7 @@ public class Main {
             }
         }
         int total = 0 ;
-        for(int i = 0 ;i<10; i++){
+        for(int i = 0 ;i<b; i++){
             total+=Math.pow(arr[i],2);
         }
         System.out.print(total);
