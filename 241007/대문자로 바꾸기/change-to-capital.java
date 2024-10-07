76c87afcb9e9f39bf3e5434ed1b1da788b4c497f
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-10-07 17:47:39 +0900

    Title: 대문자로 바꾸기, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241007/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0 ; i<5; i++){
+            for(int j = 0; j<3; j++){
+                char a = sc.next().charAt(0);
+                System.out.print(Character.toUpperCase(a)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
