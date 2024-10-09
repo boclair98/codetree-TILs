735605dc0a4fd2be5533fa91aa162275d3478ca2
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-10-09 21:23:54 +0900

    Title: 격자로 사각형 만들기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241009/격자로 사각형 만들기/print-grid-in-rectangle.java
+++ 241009/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -16,7 +16,7 @@ public class Main {
             }
         }
         for(int i = 0; i<n; i++){
-            for(int 0 =1 ;j<n; j++){
+            for(int j=0 ; j<n; j++){
                 System.out.print(arr[i][j]+" ");
             }
             System.out.println();
