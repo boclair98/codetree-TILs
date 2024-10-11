7f0478d9cc1431b8a46efa5592fbc810de853727
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-10-11 17:43:05 +0900

    Title: 가장 짧은 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241011/가장 짧은 문자열/shortest-string.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String f = sc.next();
+        String w = sc.next();
+        String t = sc.next();
+        int max_val = sf ;
+        int min_val = sf;
+
+        int sf = f.length();
+        int sw = w.length();
+        int st = t.length();
+        if(sf<sw){
+            max_val = sw;
+        }
+        if(sw<st){
+            max_val = st;
+        }
+
+        if(sw>min_val){
+            min_val = sw;
+        }
+        if(st>min_val){
+            min_val = st;
+        }
+        System.out.print(max_val-min_val);
+    }
+}
