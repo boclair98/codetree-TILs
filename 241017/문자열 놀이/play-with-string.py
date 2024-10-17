a2abeb02a48056a5888966d3febbc62360b0d616
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-10-17 22:17:34 +0900

    Title: 문자열 놀이, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/문자열 놀이/play-with-string.py
@@ -0,0 +1,19 @@
+s,n = input().split()
+n = int(n)
+s = list(s)
+
+for _ in range(n):
+    a,b,c = map(str,input().split())
+    if a == '1':
+        b = int(b)
+        c = int(c)
+        temp = s[b-1]
+        s[b-1] = s[c-1]
+        s[c-1] = temp 
+        print("".join(map(str,s)))
+    elif a == '2':
+        for i in s:
+            if i == b:
+                print(c,end='')
+            else:
+                print(i,end='')
