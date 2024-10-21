f00e097f4b441e672b4a5c3e125e11c271b5efd8
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-10-22 00:07:54 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241022/문자 제거하기/del-char.py
@@ -0,0 +1,11 @@
+s = input()
+while True:
+    idx = int(input())
+    if idx > len(s):
+        s = s[:len(s)-1]
+        print(s)
+    else:
+        s = s[:idx] + s[idx+1:]
+        print(s)
+    if len(s) == 1:
+        break
