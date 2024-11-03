eda086b46b0c49c754911fa48dd544f68bb06795
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-03 15:01:52 +0900

    Title: 붙여서 합하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241103/붙여서 합하기/add-and-add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String f = sc.next();
+        String s = sc.next();
+        String str = f + s;
+        String str2 = s + f;
+        System.out.print(Integer.ParseInt(str)+Integer.ParseInt(str2));
+    }
+}
