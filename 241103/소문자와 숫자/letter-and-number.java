3bee46d446c2ecf0b66f1d4590b3c41a44045a16
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-03 13:58:57 +0900

    Title: 소문자와 숫자, Time: 121ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241103/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        for(int i = 0 ; i<word.length(); i++){
+            if( Character.isDigit(word.charAt(i)) == true ){
+                System.out.print(word.charAt(i));
+            }else if(word.charAt(i)>='a' && word.charAt(i)<='z'){
+                System.out.print(word.charAt(i));
+            }else if(word.charAt(i)>='A' && word.charAt(i)<='Z'){
+                System.out.print(Character.toLowerCase(word.charAt(i)));
+            }
+        }
+    }
+}
