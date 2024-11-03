6c8bdbfce91c0dd422769f75e8f6c6bdff39f4d8
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-03 12:13:06 +0900

    Title: 그 다음 알파벳, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241103/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        char word = sc.next().charAt(0);
+        if(word =='z'){
+            System.out.print('a');
+        }else{
+            int word1 = (int) word + 1;
+            System.out.print((char) word1);
+        }
+    }
+}
