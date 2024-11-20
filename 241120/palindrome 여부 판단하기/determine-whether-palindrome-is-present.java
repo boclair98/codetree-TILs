bd8c2810ff0c948470e80b8102e1cf9783858812
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-20 13:54:54 +0900

    Title: palindrome 여부 판단하기, Time: 113ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241120/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static boolean check(String s, char[] arr){
+        for (int i = s.length(); i>0; i--){
+            arr[s.length()-i] = s.charAt(s.length()-i);
+        }
+        boolean isarr = false;
+
+        for(int i = 0; i<arr.length; i++){
+            if (arr[i]!=s.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] arr = new char[s.length()];
+
+        boolean ss = check(s,arr);
+        if(ss ==true){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
+    }
+}
