82151d7f29a99f059a0e20033b1afcd200c63628
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-20 11:58:59 +0900

    Title: 두 수의 거듭제곱, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241120/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static int gl(int num, int num2){
+        int tal = 1 ; 
+        for(int i = 1; i<=num2; i++){
+            tal *=num;
+            
+        }
+        return tal ;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(gl(a,b));
+    }
+}
