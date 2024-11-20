78ea2b852b81ede4ac81a43ae91d80f558e89513
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-20 14:43:27 +0900

    Title: 2개 이상의 알파벳, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

--- 241120/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 241120/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     
     public static boolean chage(String s){
-        int total = 0 ;
+        int total = 1 ;
 
         for(int i = 0; i<s.length()-1; i++){
             if (s.charAt(i)!=s.charAt(i+1)){
