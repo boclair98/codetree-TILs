6eefe373ddd1386ca7fdee6b0603768772edc3f6
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-28 00:31:59 +0900

    Title: 요일 맞추기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241128/요일 맞추기/guess-day-of-week.java
+++ 241128/요일 맞추기/guess-day-of-week.java
@@ -4,51 +4,34 @@ public class Main {
     private static String[] days = new String[]{"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
     private static int m1,d1,m2,d2,idx;
 
+    public static int calucion(int a, int b){
+        return day[a] + b; 
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         m1 = sc.nextInt();
         d1 = sc.nextInt();
         m2 = sc.nextInt();
         d2 = sc.nextInt();
-
-        if(m1 == m2){
-            idx = 1;
-            if(d1 > d2){
-                while (true){
-                    if(d1 == d2){
-                        break;
-                    }
-                
-                    if(d1<1){
-                        m1--;
-                        d1 = day[m1];
-                    }
-                    if(idx<0){
-                        idx = 6; 
-                    }
-                    d1--;
-                    idx--;
+        int idx = 1;
+        int first = calucion(m1,d1);
+        int second = calucion(m2,d2);
+        if(first > second){
+            int total = first - second;
+            while (true){
+                if(total == 0){
+                    break;
                 }
-            }else{
-                while (true){
-                    if(d1 == d2){
-                        break;
-                    }
-                
-                    if(d1<day[m1]){
-                        m1++;
-                        d1 = 1;
-                    }
-                    if(idx>6){
-                        idx = 0; 
-                    }
-                    d1++;
-                    idx++;
+                total--;
+                idx--;
+                if(idx<0){
+                    idx = 6;
                 }
-
             }
             System.out.println(days[idx]);
-            
+        }else{
+            int total = second - first;
+            System.out.print(days[total%7]);
         }
         
     }
