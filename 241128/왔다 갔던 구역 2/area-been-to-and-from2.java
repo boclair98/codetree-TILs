eac37629f5e7201f2ed27ac5e76b0145c147a20b
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-28 14:40:47 +0900

    Title: 왔다 갔던 구역 2, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241128/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[201];
+        int now = 100;
+        for(int i = 0; i<n; i++){
+            int a = sc.nextInt();
+            char word = sc.next().charAt(0);
+            if(word == 'R'){
+                for(int s =now; s<now+a; s++){
+                    arr[s] = arr[s] + 1;
+                }
+                now = now + a;
+            }else{
+                for(int s = now; s > now-a; s--){
+                    arr[s] = arr[s] + 1;
+                }
+                now = now - a;
+            }
+        }
+        int count = 0;
+        for(int i =1; i<201; i++){
+            if(arr[i]>=2){
+                count++;
+            }
+        }
+        System.out.print(count);
+
+                
+    }
+}
