902a8d1a7aa648e59d5cb4891507d448af065d59
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-28 13:57:41 +0900

    Title: 최대로 겹치는 구간, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241128/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 241128/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -14,8 +14,8 @@ public class Main {
                 arr[s] = arr[s] + 1;
             }
         }
-        int max_val = 0;
-        for(int i = 0; i<201; i++){
+        int max_val = -1;
+        for(int i = 1; i<201; i++){
             if(arr[i]> max_val){
                 max_val = arr[i];
             }
