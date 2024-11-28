4716b0a356c38e08144369b705d7647cb347e64e
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-11-28 11:08:58 +0900

    Title: 그 요일은, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241128/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    private static int m1,d1,m2,d2,cnt;
+    private static String day;
+    private static int[] arr = new int[]{0,31,29,31,30,31,30,31,31,30,31,30,31};
+    private static String[] days = new String[]{"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        m1 = sc.nextInt();
+        d1 = sc.nextInt();
+        m2 = sc.nextInt();
+        d2 = sc.nextInt(); 
+        day = sc.next();
+        int idx = 0;
+
+        while(true){
+            if(m1 == m2 && d1 == d2){
+                break;
+            }
+            
+            d1++;
+            idx++;
+            if(d1 > arr[m1]){
+                m1++;
+                d1 = 1;
+            }
+            if(days[idx%7] == "Sat"){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
