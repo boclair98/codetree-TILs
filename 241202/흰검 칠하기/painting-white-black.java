276d3e7eebada4fc4f570c4b6c147d28c89d695a
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-02 02:29:51 +0900

    Title: 흰검 칠하기, Time: 139ms, Memory: 10MB, Status: Runtime Error - CodeTree

--- 241202/흰검 칠하기/painting-white-black.java
+++ 241202/흰검 칠하기/painting-white-black.java
@@ -1,46 +1,41 @@
 import java.util.*;
 public class Main {
-    private static int[] color = new int[2001];
-    private static String[] colors = new String[2001];
+    private static String[] color = new String[2001];
+    private static int[] blackcolor = new int[2001];
+    private static int[] yellowcolor = new int[2001];
     private static int[] first = new int[2001];
     private static int white,black,gray,now;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         now = 1000;
         for(int s = 0; s<n; s++){
             int number = sc.nextInt();
             char word = sc.next().charAt(0);
             if(word =='R'){
                 for(int i = now; i<now+number; i++ ){
-                    color[i] = color[i] + 1;
-                    if (colors[i] != "black"){
-                        first[i] = first[i]+1;
-                        colors[i] = "black";
-                    }
-                    
-                    
+                    blackcolor[i] = blackcolor[i]+1;
+                    color[i] ="black";
+
                 }
                 now = now + number-1;
             }else{
                 for(int i = now; i>now-number; i--){
-                    color[i] = color[i] + 1;
-                    if (colors[i] != "white"){
-                        first[i] = first[i]+1;
-                        colors[i] = "white";
-                    }
+                    yellowcolor[i] = yellowcolor[i]+1;
+                    color[i] ="white";
                 }
                 now = now - number+1;
             }
         }
 
         for(int i = 0; i<2001; i++){
-            if(first[i]>=4){
+            if(blackcolor[i]>=2 && yellowcolor[i]>=2){
                 gray++;
-            }else if(colors[i]=="white"){
-                white++;
-            }else if(colors[i]=="black"){
+            }else if(color[i]=="black"){
                 black++;
+            }else if(color[i]=="white"){
+                white++;
             }
         }
         System.out.println(white+" "+black+" "+gray);
