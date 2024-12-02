d49d0bab2a67befd09d2ad7fd9c427e802913b33
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-02 14:00:31 +0900

    Title: 사각형의 총 넓이 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241202/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 241202/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -5,13 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        int now = 100;
         for(int q = 0; q<n; q++){
             int x1 = sc.nextInt();
             int y1 = sc.nextInt();
             int x2 = sc.nextInt();
             int y2 = sc.nextInt();
-            for(int i = x1; i<x2; i++){
-                for(int j = y1; j<y2; j++){
+            for(int i = now+x1; i<now+x2; i++){
+                for(int j = now+y1; j<now+y2; j++){
                     square[i][j] = square[i][j] + 1;
                 }
             }
