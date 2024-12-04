369687d2b7a712eece7e6dd3c3103245d0285c76
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-04 14:08:12 +0900

    Title: T를 초과하는 연속 부분 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241204/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static int n,t,reset;
+    public static int[] num = new int[2001];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        t = sc.nextInt();
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        reset = 1;
+        int max_val = 1; 
+        for(int i = 1; i<n; i++){
+            if(arr[i]>t && arr[i-1]>t){
+                if(arr[i]>arr[i-1]){
+                    reset++;
+                }else{
+                    reset = 1;
+                }
+            }else{
+                reset = 1;
+            }
+            max_val = Math.max(max_val,reset);
+        }
+        System.out.println(max_val);
+    }
+}
