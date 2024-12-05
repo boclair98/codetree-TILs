3dc1d8bee0d770caf48b1ee1bfcbe6ce958cef92
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-05 12:22:53 +0900

    Title: 만나는 그 순간, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241205/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class Main {
+    public static int n,m,cnt,cnt2;
+    public static int[] a = new int[2001];
+    public static int[] b = new int[2001];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int now = 1000;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int s = 0; s<n; s++){
+            char word = sc.next().charAt(0);
+            int num = sc.nextInt();
+            if(word == 'R'){
+                for(int i = now; i<now+num; i++){
+                    a[i] =cnt;
+                    cnt++;
+                }
+                now = now + num ;
+            }else{
+                for(int i = now; i<now+num; i++){
+                    a[i] = cnt;
+                    cnt--;
+                }
+                now = now + num ;
+            }
+        }
+        int now2 = 1000;
+        for(int s = 0; s<m; s++){
+            char word = sc.next().charAt(0);
+            int num = sc.nextInt();
+            if(word == 'R'){
+                for(int i = now2; i<now2+num; i++){
+                    b[i] =cnt2;
+                    cnt2++;
+                }
+                now2 = now2 + num ;
+            }else{
+                for(int i = now2; i<now2+num; i++){
+                    b[i] = cnt2;
+                    cnt2--;
+                }
+                now2 = now2 + num ;
+            }
+        }
+        boolean check = false;
+        for(int i = 0; i<2001; i++){
+            if(a[i]>0 && b[i] > 0){
+                if(a[i] == b[i]){
+                    System.out.println(i-1000);
+                    check = true;
+                    break;
+                }
+            }
+        }
+        if(!check){
+            System.out.println(-1);
+        }
+        
+
+    }
+}
