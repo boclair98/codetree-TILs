6cf784d5a06552ad3be94ef57aebc4b8677200fc
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-08 15:15:41 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 82ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241208/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241208/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -22,10 +22,7 @@ while k > 0 :
                 people[x] = 1
                 people[c] = 1
                 k-=1
-        else:
-            people[x] = 1
-            people[c] =1
-            k-=1
+        
     if x == p:
         virus[p]-=1
     if c == p:
