8f82be7b890d083b863bbb7933421ed7c5d3c333
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-08 14:57:02 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 52ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241208/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,23 @@
+from collections import deque
+# n명의 개발자들이 있으며 t번에 걸쳐 t초에 x개발자가 y개발자와 악수
+n,k,p,t = map(int,input().split())
+people = [0 for _ in range(n+1)]
+people[p] = 1
+num = []
+for _ in range(t):
+    a,b,c = map(int,input().split())
+    num.append([a,b,c])
+num.sort(key = lambda x:x[0])
+num = deque(num)
+while k > 0 : 
+    z,x,c = num.popleft()
+    if (people[x] == 1 or people[c] == 1):
+        people[x] = 1
+        people[c] = 1
+        k -=1
+
+for i in range(1,len(people)):
+    print(people[i],end='')
+
+
+
