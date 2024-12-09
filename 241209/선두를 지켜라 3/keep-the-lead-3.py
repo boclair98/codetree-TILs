8dcb4e55a4764a916250a9de76ef77d525866adb
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-09 12:35:33 +0900

    Title: 선두를 지켜라 3, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241209/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,40 @@
+n,m = map(int,input().split())
+a = [0]*1001
+idx = 0 
+b = [0]*1001
+idx2 = 0
+for _ in range(n):
+    v,t = map(int,input().split())
+    for i in range(idx+1,t+idx+1):
+        a[idx] = a[idx-1]+ v
+        idx+=1
+
+
+for _ in range(m):
+    v,t = map(int,input().split())
+    for i in range(idx2+1,t+idx2+1):
+        b[idx2] = b[idx2-1]+ v
+        idx2+=1
+
+rank = [] 
+for i in range(1,1001):
+    if a[i] > b[i]:
+        if len(rank) == 0:
+            rank.append('a')
+        elif rank[-1] !='a':
+            rank.append('a')
+        
+    elif b[i] > a[i]:
+        if len(rank) == 0:
+            rank.append("b");
+        elif rank[-1] !='b':
+            rank.append('b')
+        
+    elif b[i] == a[i] and a[i] >0:
+        if len(rank) == 0:   
+            rank.append(("a","b"))
+        elif rank[-1] != ("a","b"):
+            rank.append(("a","b"))
+    elif b[i] == 0 and a[i] == 0:
+        break
+print(len(rank))
