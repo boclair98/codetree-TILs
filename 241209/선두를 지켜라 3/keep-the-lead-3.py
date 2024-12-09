c82404a5812a695096926756375a6ac2f671a421
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-09 12:54:51 +0900

    Title: 선두를 지켜라 3, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241209/선두를 지켜라 3/keep-the-lead-3.py
+++ 241209/선두를 지켜라 3/keep-the-lead-3.py
@@ -1,40 +1,38 @@
 n,m = map(int,input().split())
 a = [0]*10001
-idx = 0 
+idx = 1 
 b = [0]*10001
-idx2 = 0
+idx2 = 1
 for _ in range(n):
     v,t = map(int,input().split())
-    for i in range(idx+1,t+idx+1):
-        a[i] = a[i-1]+ v
+    for i in range(idx,idx+t):
+        a[i] = a[i-1] + v
         idx+=1
-
 
 for _ in range(m):
     v,t = map(int,input().split())
-    for i in range(idx2+1,t+idx2+1):
-        b[i] = b[i-1]+ v
+    for i in range(idx2,idx2+t):
+        b[i] = b[i-1] + v
         idx2+=1
 
-rank = [] 
+rank = []
+
 for i in range(1,1001):
-    if a[i] > b[i]:
-        if len(rank) == 0:
+    if len(rank) == 0:
+        if a[i] > b[i]:
             rank.append('a')
-        elif rank[-1] !='a':
-            rank.append('a')
-        
-    elif b[i] > a[i]:
-        if len(rank) == 0:
-            rank.append("b");
-        elif rank[-1] !='b':
+        elif a[i] < b[i]:
             rank.append('b')
-        
-    elif b[i] == a[i] and a[i] >0:
-        if len(rank) == 0:   
-            rank.append(("a","b"))
-        elif rank[-1] != ("a","b"):
-            rank.append(("a","b"))
-    elif b[i] == 0 and a[i] == 0:
-        break
+        else:
+            rank.append(('a','b'))
+    elif len(rank) > 0:
+        if (a[i] > b[i]):
+            if rank[-1] =='b' or rank[-1] ==("a","b"):
+                rank.append('a')
+        elif a[i] < b[i]:
+            if rank[-1] =='a' or rank[-1] ==("a","b"):
+                rank.append('b')
+        else:
+            if(rank[-1] == 'a' or rank[-1]) =='b':
+                rank.append(('a','b'))
 print(len(rank))
