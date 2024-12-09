8926c845e3221b5c1caa0d0cd1ec89a0a8cc756f
boclair98 <108774323+boclair98@users.noreply.github.com>
2024-12-09 15:54:29 +0900

    Title: 작은 구슬의 이동, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241209/작은 구슬의 이동/small-marble-movement.py
+++ 241209/작은 구슬의 이동/small-marble-movement.py
@@ -9,51 +9,52 @@ if alpha =='R':
     while t > 0:
         if shape == 'Right':
             y+=1
-            if y == n-1:
+            if y == n:
                 t -=1
                 shape ='Left'
             else:
                 t-=1
         else:
             y-=1
-            if y == 0:
+            if y == 1:
                 t-=1
                 shape ="Right"
             else:
                 t-=1 
-    print(x,y+1)
+    print(x,y)
 elif alpha == "L":
     shape = "Left"
     while t>0:
         if (shape == "Left"):
             y-=1
-            if y == 0:
+            t-=1
+            if y == 1:
                 t-=1
                 shape = 'Right'
-            else:
-                t-=1
+            
         else:
             y+=1
-            if y == n-1:
+            t-=1
+            if y == n:
                 t-=1
                 shape = 'Left'
-            else:
-                t-=1
-    print(x,y+1)
+            
+
+    print(x,y)
 elif alpha == 'U':
     shape = "up"
     while t > 0:
         if (shape == 'up'):
             x-=1
-            if x == 0:
-                t-=1
+            if x == 1:
+                t-=2
                 shape ="down"
             else:
                 t-=1
         else:
             x+=1
-            if x == n-1:
-                t-=1
+            if x == n:
+                t-=2
                 shape = "up"
             else:
                 t-=1
@@ -63,15 +64,15 @@ elif alpha == 'D':
     while t > 0:
         if (shape == 'down'):
             x+=1
-            if x == n-1:
-                t-=1
+            if x == n:
+                t-=2
                 shape ="up"
             else:
                 t-=1
         else:
             x-=1
-            if x == 0:
-                t-=1
+            if x == 1:
+                t-=2
                 shape = "down"
             else:
                 t-=1
