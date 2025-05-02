5a7b94431b8df82f34737494bccd47c583f8eec3
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-05-02 15:11:58 +0900

    Title: 스승의 은혜 3, Time: 107ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250502/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, s, total;
+    static int[] arr1;
+    static int[] arr2;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        arr1 = new int[n];
+        arr2 = new int[n];
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            arr1[i] = Integer.parseInt(st.nextToken());
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i<n; i++){
+            int count = 1;
+            int cost = s;
+            int cnt = 0;
+            for(int j = i; j<n; j++){
+                if(count > 0){
+                    count--;
+                    cost-=(arr1[i]/2);
+                    cost-=arr2[i];
+                    if(cost < 0){
+                        break;
+                    }else{
+                        cnt++;
+                    }
+                }else{
+                    cost-=arr1[i];
+                    cost-=arr2[i];
+                    if(cost<0){
+                        break;
+                    }else{
+                        cnt++;
+                    }
+                }
+            }
+            total = Math.max(total,cnt);
+        }
+        System.out.println(total);
+    }
+
+
+
+
+}
