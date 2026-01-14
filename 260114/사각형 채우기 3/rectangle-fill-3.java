fe054c7b618f26c13336ac0a126d4f8f9de5d046
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-01-14 14:30:42 +0900

    Title: 사각형 채우기 3, Time: 93ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260114/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb;
+    static StringTokenizer st;
+    static int n, count;
+    static int[] dp = new int[1010];
+    static int recur(int size){
+        if(dp[size] != -1) return dp[size];
+        if(size == 1){
+            return 2;
+        }
+        if(size == 2){
+            return 7;
+        }
+        dp[size] = recur(size - 1) * 3 + 1;
+        return dp[size];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        Arrays.fill(dp,-1);
+        System.out.println(recur(n));
+    }
+}
