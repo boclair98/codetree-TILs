ae46caf3ec1f84e9e7d72a5226031c0e402c3240
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-01-14 14:22:19 +0900

    Title: 계단 오르기, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260114/계단 오르기/climbing-stairs.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb;
+    static StringTokenizer st;
+    static int n, count;
+    static int[] dp = new int[1010];
+    static int recur(int step){
+        if(step > n) return 0;
+        if(step == n) return 1;
+        if(dp[step] != -1) return dp[step];
+        int ans = 0;
+        ans += recur(step + 2);
+        ans += recur(step + 3);
+        return dp[step] = ans;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        Arrays.fill(dp,-1);
+        System.out.println(recur(0));
+    }
+}
